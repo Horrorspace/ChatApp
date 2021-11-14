e0be4d5958628e4346dfa91b0a509468ac4f3940
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-14 16:38:27 +0700

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all',
+        }
+    };
+
+    if(isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin
+        ]
+    }
+
+    return config
+};
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname),
+    entry: {
+        app: './src/client/index.tsx',
+    },
+    output: {
+        filename: 'public/js/[name].js',
+        path: path.resolve(__dirname, './build/src/client'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Chat App',
+            template: './src/client/assets/html/template.html',
+            scriptLoading: 'defer',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/client/assets/img/ico/favicon.ico'),
+                    to: path.resolve(__dirname, 'build/src/client/public/ico')
+                }
+            ],
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'public/css/[name].css'
+        }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json', 'css', '.less', '.html'],
+        alias: {
+            '@api': path.resolve(__dirname, './src/client/api'),
+            '@config': path.resolve(__dirname, './src/client/config'),
+            '@core': path.resolve(__dirname, './src/client/core'),
+            '@store': path.resolve(__dirname, './src/client/store'),
+            '@styles': path.resolve(__dirname, './src/client/assets/styles'),
+            '@types': path.resolve(__dirname, './src/client/types'),
+            '@ui': path.resolve(__dirname, './src/client/ui'),
+            '@utils': path.resolve(__dirname, './src/client/utils'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                },
+                'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(tsx?)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "ts-loader",
+                }
+            },
+        ],
+    },
+    optimization: optimization()
+}
