dd82a21c9196523d6e9468564443a36a34013931
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-15 20:02:50 +0700

    Create Messages component

---
+++ src/client/ui/components/chat/Messages.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Row} from 'antd';
+import {Message} from '@ui/components/chat/Message';
+import {message} from '@interfaces/IMessage';
+
+
+interface messagesProps {
+    messages: message[]
+}
+
+
+const Component: React.FC<messagesProps> = ({messages}: messagesProps) => {  
+   const messageProd = ({text, fromUser, time}: message): React.ReactElement => {
+       return (
+           <Row
+            align="middle"
+            justify={fromUser ? 'end' : 'start'}
+           >
+               <Message
+                text={text}
+                time={time}
+               />
+           </Row>
+       )
+   }
+   
+   const renderedMessages: React.ReactElement[] = messages.map(messageProd)
+
+
+    return (
+        <>
+            {renderedMessages}
+        </>
+    )
+}
+
+export const Messages = React.memo(Component);
