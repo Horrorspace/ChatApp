d82a0d1acf765b7fbd5e014ef5ca0ccf96a8b560
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-15 20:03:18 +0700

    Create MessageEditor component

---
+++ src/client/ui/components/chat/MessageEditor.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Row, Col, Button, Input} from 'antd';
+import {SendOutlined} from '@ant-design/icons';
+import {Сhat} from '@lang/en/Сhat';
+
+const Component: React.FC = () => { 
+    return (
+        <Row
+        align="middle"
+        justify="center"
+        >
+            <Col>
+                <Input 
+                    placeholder={Сhat.messagePlaceholder}
+                />
+            </Col>
+            <Col>
+                <Button 
+                    type="primary"
+                >
+                    {Сhat.send}
+                    <SendOutlined />
+                </Button>
+            </Col>
+        </Row>
+    )
+}
+
+export const MessageEditor = React.memo(Component);
