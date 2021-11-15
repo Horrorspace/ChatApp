340750020efad1a173153d2e9ece106796110388
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-15 19:58:28 +0700

    Add App component in render

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import {render} from 'react-dom'
+import 'antd/dist/antd.css';
+import React from 'react';
+import {render} from 'react-dom';
+import {App} from '@ui/App'
 
 
-render(<div />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
