61b10c93ee4176e9ba8cce058e6745eb579e935d
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-15 20:03:04 +0700

    Create contact component

---
+++ src/client/ui/components/chat/Contact.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Row, Col, Image} from 'antd';
+import {Сhat} from '@lang/en/Сhat';
+import {lastOnline} from '@aliases/Message'
+
+
+
+interface contactProps {
+    username: string;
+    avatarSrc: string;
+    online: boolean;
+    lastOnline: lastOnline;
+}
+
+
+const Component: React.FC<contactProps> = ({
+    username,
+    avatarSrc,
+    online,
+    lastOnline
+}: contactProps) => {
+    const getOnlineStr = (online: boolean, lastOnline: lastOnline): string => {
+        if(online){
+            return Сhat.online
+        }
+        else if(lastOnline) {
+            return `${Сhat.lastSeen} ${lastOnline.toString()}`
+        }
+        else {
+            return Сhat.offline
+        }
+    }
+    const onlineStr = getOnlineStr(online, lastOnline);
+
+    return (
+        <Row
+        align="middle"
+        justify="center"
+        >
+            <Col>
+                <Image 
+                    src={avatarSrc}
+                    alt={Сhat.avatarAlt}
+                    width={40}
+                    height={40}
+                    preview={false}
+                />
+            </Col>
+            <Col>
+                <Row>
+                    {username}
+                </Row>
+                <Row>
+                    {onlineStr}
+                </Row>
+            </Col>
+        </Row>
+    )
+}
+
+export const Contact = React.memo(Component);
