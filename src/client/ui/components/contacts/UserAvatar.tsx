2b8eaac319a85c47ecab4da3afe991e78d8b7f57
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-15 21:53:47 +0700

    Change type from ElementCreator to React.FC, replace to components folder

--- src/client/ui/components/contacts/UserAvatar.tsx
+++ src/client/ui/components/contacts/UserAvatar.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Col, Image} from 'antd';
 import {Сhat} from '@lang/en/Сhat';
-import {ElementCreator} from '@aliases/ui';
 import {userAvatarProps} from '@interfaces/IProps';
 
 
-export const userAvatar: ElementCreator<userAvatarProps> = ({lastMessagefromUser, userAvatarSrc}: userAvatarProps) => {
+export const Component: React.FC<userAvatarProps> = ({lastMessagefromUser, userAvatarSrc}: userAvatarProps) => {
     if(lastMessagefromUser) {
         return (
                 <Col>
@@ -26,3 +25,5 @@ export const userAvatar: ElementCreator<userAvatarProps> = ({lastMessagefromUser
         )
     }
 }
+
+export const UserAvatar = React.memo(Component);
