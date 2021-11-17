e2d8c7a993841b17764067e48bbcb380650ebc2f
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-17 21:43:51 +0700

    update Chat enum

--- src/client/lang/en/Сhat.ts
+++ src/client/lang/en/Сhat.ts
@@ -1,9 +1,11 @@
 export enum Сhat {
     avatarAlt = "User avatar",
+    email="Email",
     lastSeen="Last seen online",
     messagePlaceholder="Message",
     searchPlaceholder="Search",
     online="Online",
     offline="Offline",
     send="Send",
+    username="Username"
 }
