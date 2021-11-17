57970393906c88a039f4433b00bf09a0448635c3
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-17 21:44:23 +0700

    Create Profile page

--- src/client/ui/pages/ProfilePage.tsx
+++ src/client/ui/pages/ProfilePage.tsx
@@ -1,34 +1,44 @@
 import React from 'react';
 import {Row, Image} from 'antd';
 import {Сhat} from '@lang/en/Сhat';
+import {InfoList} from '@ui/components/profile/InfoList'
+import {IInfoListProps, IInfoFieldProps} from '@interfaces/IProps';
 
 
 export const ProfilePage: React.FC = () => {
     const avatarSrc: string = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album"
-    const username="Just for lulz";
+    const username: string ="Just for lulz";
+    const email: string = "justforlulz@google.com";
+    const usernameField: IInfoFieldProps = {
+        title: Сhat.username,
+        value: username
+    }
+    const emailField: IInfoFieldProps = {
+        title: Сhat.email,
+        value: email
+    }
+    const infoList: IInfoListProps = {
+        infoFields: [usernameField, emailField]
+    }
 
 
     return (
         <>
-            <Image 
-                src={avatarSrc}
-                alt={Сhat.avatarAlt}
-                width={200}
-                height={200}
-                preview={false}
+            <Row
+                align="middle"
+                justify="center"
+            >
+                <Image 
+                    src={avatarSrc}
+                    alt={Сhat.avatarAlt}
+                    width={200}
+                    height={200}
+                    preview={false}
+                />
+            </Row>
+            <InfoList 
+                infoFields={infoList}
             />
-            <Row
-                align="middle"
-                justify='space-between'
-            >
-                {""}
-            </Row>
-            <Row
-                align="middle"
-                justify='space-between'
-            >
-                {username}
-            </Row>
         </>
     )
 }
