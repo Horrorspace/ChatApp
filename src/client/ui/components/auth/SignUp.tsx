b33cb78fc81df03102f6b4f502d10898202a5f47
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-18 22:06:27 +0700

    Create sign up component

---
+++ src/client/ui/components/auth/SignUp.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Row, Col, Button, Divider} from 'antd';
+import {Chat} from '@lang/en/Chat';
+import {InputField} from '@ui/components/auth/InputField';
+import {PassField} from '@ui/components/auth/PassField';
+
+
+const Component: React.FC = () => {
+    const userNameTitle: string = `${Chat.username}:`;
+    const userNamePlaceholder: string = Chat.username.trim();
+
+    const EmailTitle: string = `${Chat.email}:`;
+    const EmailPlaceholder: string = 'user@site.com';
+
+    const passTitle: string = `${Chat.password}:`;
+    const passPlaceholder: string = 'password';
+
+    return (
+        <>
+            <Divider />
+            <InputField 
+                title={userNameTitle}
+                placeholder={userNamePlaceholder}
+            />
+            <InputField 
+                title={EmailTitle}
+                placeholder={EmailPlaceholder}
+            />
+            <PassField 
+                title={passTitle}
+                placeholder={passPlaceholder}
+            />
+            <Row
+                align="middle"
+                justify="center"    
+            >
+                <Col>
+                    <Button>
+                        {Chat.createAccount}
+                    </Button>
+                </Col>
+            </Row>
+            <Divider />
+        </>
+    )
+}
+
+export const SignUp = React.memo(Component);
