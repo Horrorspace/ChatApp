1867c2fb73ac0c7e1c4fed0a1751ffc8cba0a249
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-18 22:06:50 +0700

    Change render page on register page

--- src/client/ui/App.tsx
+++ src/client/ui/App.tsx
@@ -3,7 +3,8 @@ import {Layout} from 'antd';
 // import {ChatPage} from '@ui/pages/ChatPage';
 // import {ContactsPage} from '@ui/pages/ContactsPage';
 // import {ProfilePage} from '@ui/pages/ProfilePage';
-import {AuthPage} from '@ui/pages/AuthPage';
+// import {AuthPage} from '@ui/pages/AuthPage';
+import {RegisterPage} from '@ui/pages/RegisterPage';
 
 
 const {Header, Content, Footer} = Layout;
@@ -14,7 +15,7 @@ export const App: React.FC = () => {
         <Layout>
             <Header></Header>
             <Content>
-                <AuthPage />
+                <RegisterPage />
             </Content>
             <Footer></Footer>
         </Layout>
