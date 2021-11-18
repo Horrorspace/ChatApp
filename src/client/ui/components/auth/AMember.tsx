2d7dd72b7f81da87f39898d9d4bd8f7f4768784f
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-18 22:00:00 +0700

    Rename component and add props

--- src/client/ui/components/auth/AMember.tsx
+++ src/client/ui/components/auth/AMember.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Row, Col, Button, Typography} from 'antd';
 import {Chat} from '@lang/en/Chat';
+import {IAMemberProps} from '@interfaces/IProps'
 
 
-const Component: React.FC = () => {
+const Component: React.FC<IAMemberProps> = ({aMember}: IAMemberProps) => {
     const {Title} = Typography;
-    const title: string = `${Chat.notAMember}? ${Chat.signUp} ${Chat.now}:`;
+
+    const signInTitle: string = `${Chat.notAMember}? ${Chat.signUp} ${Chat.now}:`;
+    const signUpTitle: string = `${Chat.alreadyAMember}? ${Chat.signIn}:`;
+    const title: string = aMember ? signInTitle : signUpTitle;
+
+    const signInButton: string = Chat.signUp;
+    const signUpButton: string = Chat.signIn;
+    const buttonText: string = aMember ? signInButton : signUpButton;
 
     return (
         <>
@@ -23,7 +31,7 @@ const Component: React.FC = () => {
             >
                 <Col>
                     <Button>
-                        {Chat.signUp}
+                        {buttonText}
                     </Button>
                 </Col>
             </Row>
@@ -31,4 +39,4 @@ const Component: React.FC = () => {
     )
 }
 
-export const NotAMember = React.memo(Component);
+export const AMember = React.memo(Component);
