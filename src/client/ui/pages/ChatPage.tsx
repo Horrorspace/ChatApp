1b20d53d638a2f48add94508fd223ffe3bfb21cd
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-21 09:23:22 +0700

    change test messages

--- src/client/ui/pages/ChatPage.tsx
+++ src/client/ui/pages/ChatPage.tsx
@@ -32,6 +32,21 @@ export const ChatPage: React.FC = () => {
             fromUser: false,
             time: new Date('November 14, 2021 19:28:00')
         },
+        {
+            text: 'Six',
+            fromUser: true,
+            time: new Date('November 14, 2021 19:28:00')
+        },
+        {
+            text: 'Seven \n Seven',
+            fromUser: false,
+            time: new Date('November 14, 2021 19:28:00')
+        },
+        {
+            text: 'Eight \n Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight',
+            fromUser: true,
+            time: new Date('November 14, 2021 19:28:00')
+        },
     ]
 
     const avatarSrc = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album"
