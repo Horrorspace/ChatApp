3a9011022c67dfee07b9684a6350fd7922dbc8fc
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-23 21:59:09 +0700

    edit test message

--- src/client/ui/components/contacts/ContactCards.tsx
+++ src/client/ui/components/contacts/ContactCards.tsx
@@ -6,6 +6,7 @@ const Component: React.FC = () => {
 
     const avatarSrc: string = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album";
     const userAvatarSrc: string = "https://sun9-20.userapi.com/impg/t062bKv86xwGI1wrgDBjs2_XePZhfAF74SrULw/UcIyPrvXlVo.jpg?size=1079x1079&quality=96&sign=f4a8be9f66adc2c3e8c538548e28f8a4&type=album";
+    const messageTest: string = 'Five \n Five Five Five Five Five Five Five Five Five Five Five Five Five Five';
 
     return (
         <section
@@ -20,7 +21,7 @@ const Component: React.FC = () => {
                     userAvatarSrc={userAvatarSrc}
                     online={true}
                     lastMessagefromUser={true}
-                    lastMessage="Five"
+                    lastMessage={messageTest}
                     timeOfLastMessage={new Date('November 14, 2021 19:28:00')}
                 />
                 <ContactCard 
