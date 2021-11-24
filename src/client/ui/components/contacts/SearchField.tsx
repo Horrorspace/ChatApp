e5232f6b1dcbfc060d1f1431d06c663f10734d3d
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-11-24 22:15:39 +0700

    add settings menu and add user modal

--- src/client/ui/components/contacts/SearchField.tsx
+++ src/client/ui/components/contacts/SearchField.tsx
@@ -1,10 +1,91 @@
-import React from 'react';
-import {Row, Col, Input} from 'antd';
-import {SearchOutlined} from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Row, Col, Input, Button, Dropdown, Menu, Typography, Modal} from 'antd';
+import {SearchOutlined, SettingOutlined, UserAddOutlined, CloseOutlined} from '@ant-design/icons';
 import {Chat} from '@lang/en/Chat';
 
 
 const Component: React.FC = () => { 
+    const {Paragraph} = Typography;
+    const [settings, setSettings] = useState(false);
+    const [add, setAdd] = useState(false);
+
+    const closeIco: React.ReactElement = (
+        <CloseOutlined 
+            className="add-modal__close-ico"
+        />
+    );
+
+    const modalFooter: React.ReactElement = (
+        <Row>
+            <Col>
+                <Button
+                    type="primary"
+                >
+                    {Chat.ok}
+                </Button>
+            </Col>
+            <Col>
+                <Button
+                    type="default"
+                    onClick={() => {setAdd(false)}}
+                >
+                    {Chat.cancel}
+                </Button>
+            </Col>
+        </Row>
+    );
+    
+    const addModal: React.ReactElement = (
+        <Modal
+            title={Chat.addUserTitle}
+            visible={add}
+            onCancel={() => {setAdd(false)}}
+            closeIcon={closeIco}
+            footer={modalFooter}
+            className=""
+            wrapClassName=""
+        >
+            <Input 
+                placeholder={Chat.addUserPlaceholder}
+                className=""
+            />
+        </Modal>
+    )
+    
+    const menu: React.ReactElement = (
+        <Menu
+            className="settings-menu-list"
+        >
+            <Menu.Item
+               className="settings-menu-item__wrap" 
+            >
+                <Paragraph
+                    className="settings-menu-item"
+                >
+                    {"test"}
+                </Paragraph>
+            </Menu.Item>
+            <Menu.Item
+               className="settings-menu-item__wrap" 
+            >
+                <Paragraph
+                    className="settings-menu-item"
+                >
+                    {"test"}
+                </Paragraph>
+            </Menu.Item>
+            <Menu.Item
+               className="settings-menu-item__wrap" 
+            >
+                <Paragraph
+                    className="settings-menu-item"
+                >
+                    {"test"}
+                </Paragraph>
+            </Menu.Item>
+        </Menu>
+    );
+    
     return (
         <section
             className="search-field__wrap"
@@ -14,6 +95,24 @@ const Component: React.FC = () => {
                 justify="center"
                 className="search-field"
             >
+                <Col
+                    className="setting-ico__wrap"
+                >
+                    <Button
+                        className="setting-btn"
+                        onClick={() => {setSettings(p => !p)}}
+                    >
+                        <Dropdown
+                            overlay={menu}
+                            className="setting-btn"
+                            visible={settings}
+                        >
+                            <SettingOutlined 
+                                className="setting-ico"
+                            />
+                        </Dropdown>
+                    </Button>
+                </Col>
                 <Col
                     className="search-ico__wrap"
                 >
@@ -29,7 +128,21 @@ const Component: React.FC = () => {
                         className="search"
                     />
                 </Col>
+                <Col
+                    className="add-ico__wrap"
+                >
+                    <Button
+                        className="add-btn"
+                        onClick={() => {setAdd(p => !p)}}
+                    >
+                        <UserAddOutlined 
+                            className="add-ico"
+                        />
+                    </Button>
+                </Col>
             </Row>
+
+            {addModal}
         </section>
     )
 }
