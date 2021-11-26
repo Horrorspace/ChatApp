0e115c374d5c3b0299e23daa4ee3a9599959fa51
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-11-26 12:55:35 +0700

    Update ProfileMenu.tsx

--- src/client/ui/components/profile/ProfileMenu.tsx
+++ src/client/ui/components/profile/ProfileMenu.tsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import {Row, Col, Typography} from 'antd';
+import {IInfoFieldProps} from '@interfaces/IProps';
 
+
+const Component: React.FC<IInfoFieldProps> = ({title, value}: IInfoFieldProps) => {
+    const {Title, Paragraph} = Typography;
+
+    return (
+        <>
+            <Row
+                align="middle"
+                justify="center"
+            >
+                <Col>
+                    <Title level={3}>{title}</Title>
+                </Col>
+            </Row>
+            <Row
+                align="middle"
+                justify="center"
+            >
+                <Col>
+                    <Paragraph>{value}</Paragraph>
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export const InfoField = React.memo(Component);
