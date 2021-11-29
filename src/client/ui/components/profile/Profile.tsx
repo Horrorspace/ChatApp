89841a59b582238b74251e67f960bde2024e2356
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-11-29 12:40:17 +0700

    Update Profile.tsx

--- src/client/ui/components/profile/Profile.tsx
+++ src/client/ui/components/profile/Profile.tsx
@@ -1,1 +1,5 @@
-
+import React from 'react';
+import {InfoList} from '@ui/components/profile/InfoList';
+import {Avatart} from '@ui/components/profile/Avatart';
+import {IInfoFieldProps} from '@interfaces/IProps';
+import {InfoFields} from '@aliases/Profile';
