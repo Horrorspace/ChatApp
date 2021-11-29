78cd08d97cf6f3f561c40fe568695f855d4c4d3f
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-11-29 08:20:35 +0700

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,2 +1,28 @@
 CREATE ROLE chat_admin WITH LOGIN PASSWORD 'KQoEgwBi';
 CREATE DATABASE chat OWNER chat_admin;
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255), 
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  online BOOLEAN DEFAULT false,
+  avatar_src VARCHAR(255) DEFAULT ''
+);
+
+CREATE TABLE messages(
+  id SERIAL PRIMARY KEY,
+  user_id SERIAL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  created TIMESTAMP WITH TIME ZONE,
+  date_to_do TIMESTAMP WITH TIME ZONE,
+  date_complete TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  date_cancel  TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  date_delete  TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  title VARCHAR(255),
+  task TEXT,
+  is_priority BOOLEAN DEFAULT false,
+  is_complete BOOLEAN DEFAULT false,
+  is_cancel BOOLEAN DEFAULT false,
+  is_delete BOOLEAN DEFAULT false
+);
