5afe597da99151ef942ec02970ba2c3632467683
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-11-29 12:54:44 +0700

    Update InfoField.tsx

--- src/client/ui/components/profile/InfoField.tsx
+++ src/client/ui/components/profile/InfoField.tsx
@@ -7,24 +7,41 @@ const Component: React.FC<IInfoFieldProps> = ({title, value}: IInfoFieldProps) =
     const {Title, Paragraph} = Typography;
 
     return (
-        <>
+        <div
+            className="profile-info-field"
+        >
             <Row
                 align="middle"
                 justify="center"
+                className="profile-info-title__main-wrap" 
             >
-                <Col>
-                    <Title level={3}>{title}</Title>
+                <Col
+                    className="profile-info-title__wrap"     
+                >
+                    <Title 
+                        level={3}
+                        className="profile-info-title"
+                     >
+                        {title}
+                    </Title>
                 </Col>
             </Row>
             <Row
                 align="middle"
                 justify="center"
+                className="profile-info-value__main-wrap"
             >
-                <Col>
-                    <Paragraph>{value}</Paragraph>
+                <Col
+                    className="profile-info-value__wrap"    
+                >
+                    <Paragraph
+                        className="profile-info-value" 
+                    >
+                        {value}
+                    </Paragraph>
                 </Col>
             </Row>
-        </>
+        </div>
     )
 }
 
