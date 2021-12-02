6c43a6a29d290ec0ecf21e825b86879f94dae81b
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-02 21:54:45 +0700

    create user model

---
+++ src/server/users/users.model.ts
@@ -0,0 +1,39 @@
+import {Model, Table, Column, DataType} from 'sequelize-typescript';
+
+interface UserCreationAttrs {
+    username: string;
+    email: string;
+    password: string;
+}
+
+interface UserAttrs extends UserCreationAttrs {
+    id: number;
+    online: boolean;
+    avatar_src: string;
+    confirmed: boolean;
+}
+
+
+@Table
+export class User extends Model<UserAttrs, UserCreationAttrs> {
+    @Column({type: DataType.INTEGER, allowNull: false, unique: true, primaryKey: true, autoIncrement: true})
+    id!: number;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    username!: string;
+
+    @Column({type: DataType.STRING, allowNull: false, unique: true})
+    email!: string;
+
+    @Column({type: DataType.STRING, allowNull: false})
+    password!: string;
+
+    @Column({type: DataType.BOOLEAN})
+    online?: boolean;
+
+    @Column({type: DataType.STRING})
+    avatar_src?: string;
+
+    @Column({type: DataType.BOOLEAN})
+    confirmed?: boolean;
+}
