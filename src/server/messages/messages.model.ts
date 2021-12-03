ce04d63bb04d7281e59522aed4a188483382c232
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-03 08:02:18 +0700

    Update messages.model.ts

--- src/server/messages/messages.model.ts
+++ src/server/messages/messages.model.ts
@@ -1,1 +1,30 @@
+import {Model, Table, Column, DataType} from 'sequelize-typescript';
 
+interface MessageCreationAttrs {
+    text: string;
+    fromUserId: string;
+    toUserId: string;
+}
+
+interface MessageAttrs extends MessageCreationAttrs {
+    id: number;
+    date: Date;
+    readed: boolean;
+}
+
+
+@Table
+export class Message extends Model<MessageAttrs, MessageCreationAttrs> {
+    
+    id!: number;
+  
+    date?: Date;
+  
+    text!: string;
+
+    readed?: boolean;
+    
+    fromUserId!: string;
+
+    toUserId!: string;
+}
