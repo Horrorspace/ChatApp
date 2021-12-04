92ee971b1d62fceb5f25d066b5e9c5b6e3f7219b
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-04 22:25:30 +0700

    create user interfaces

---
+++ src/server/users/users.types.ts
@@ -0,0 +1,35 @@
+export interface UserId {
+    id: number;
+}
+
+export interface UserCreationAttrs {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface UserAttrs extends UserCreationAttrs, UserId {
+    online: boolean;
+    avatarSrc: string;
+    confirmed: boolean;
+}
+
+export interface editUserNameOpt extends UserId {
+    username: string;
+}
+
+export interface editUserEmailOpt extends UserId {
+    email: string;
+}
+
+export interface editUserPasswordOpt extends UserId {
+    password: string;
+}
+
+export interface editUserOnlineOpt extends UserId {
+    online: boolean;
+}
+
+export interface editUserAvatarOpt extends UserId {
+    avatarSrc: string;
+}
