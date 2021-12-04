545c38e244859d4d8d9761892c04f9c68d3b2df5
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-04 22:28:12 +0700

    add Messages module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,11 +1,15 @@
 import {Module} from '@nestjs/common';
 import {SequelizeModule} from '@nestjs/sequelize';
 import {ConfigModule} from '@nestjs/config';
-import {User} from './users/users.model'
+import {User} from './users/users.model';
+import {Message} from './messages/messages.model';
 import {UsersModule} from './users/users.module';
+import {MessagesModule} from './messages/messages.module';
 
 
 @Module({
+    controllers: [],
+    providers: [],
     imports: [
         ConfigModule.forRoot({
             envFilePath: `.${process.env.NODE_ENV}.env`
@@ -17,10 +21,11 @@ import {UsersModule} from './users/users.module';
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User],
+            models: [User, Message],
             autoLoadModels: true
         }),
-        UsersModule
+        UsersModule,
+        MessagesModule
     ],
 })
 export class AppModule {}
