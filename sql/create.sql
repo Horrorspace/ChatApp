a848844d2461d6aceb3c9c9c93ad3365faad48d1
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-04 22:24:59 +0700

    commet tables creation

--- sql/create.sql
+++ sql/create.sql
@@ -2,23 +2,23 @@ CREATE ROLE chat_admin WITH LOGIN PASSWORD 'KQoEgwBi';
 CREATE DATABASE chat OWNER chat_admin;
 CREATE DATABASE chat_sessions OWNER chat_admin;
 
-CREATE TABLE User(
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(255), 
-  email VARCHAR(255) UNIQUE,
-  password VARCHAR(255),
-  online BOOLEAN DEFAULT false,
-  avatar_src VARCHAR(255) DEFAULT '',
-  confirmed BOOLEAN DEFAULT false
-);
+-- CREATE TABLE User(
+--   id SERIAL PRIMARY KEY,
+--   username VARCHAR(255), 
+--   email VARCHAR(255) UNIQUE,
+--   password VARCHAR(255),
+--   online BOOLEAN DEFAULT false,
+--   avatarSrc VARCHAR(255) DEFAULT '',
+--   confirmed BOOLEAN DEFAULT false
+-- );
 
-CREATE TABLE Message(
-  id SERIAL PRIMARY KEY,
-  date TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-  text TEXT,
-  readed BOOLEAN DEFAULT false,
-  from_user_id SERIAL,
-  FOREIGN KEY (from_user_id) REFERENCES users (id),
-  to_user_id SERIAL,
-  FOREIGN KEY (to_user_id) REFERENCES users (id)
-);
+-- CREATE TABLE Message(
+--   id SERIAL PRIMARY KEY,
+--   date TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+--   text TEXT,
+--   readed BOOLEAN DEFAULT false,
+--   fromUserId SERIAL,
+--   FOREIGN KEY (fromUserId) REFERENCES users (id),
+--   toUserId SERIAL,
+--   FOREIGN KEY (toUserId) REFERENCES users (id)
+-- );
