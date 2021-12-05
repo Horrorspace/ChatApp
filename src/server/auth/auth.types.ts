f69caf33f9cb4943b33eae51e1099b1f71baa03e
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-05 21:52:24 +0700

    create auth types, and add LoginAttrs and UserAuthData interfaces

---
+++ src/server/auth/auth.types.ts
@@ -0,0 +1,13 @@
+export interface LoginAttrs {
+    email: string;
+    password: string;
+}
+
+export interface UserAuthData {
+    id: number;
+    online: boolean;
+    avatarSrc: string;
+    confirmed: boolean;
+    username: string;
+    email: string;
+}
