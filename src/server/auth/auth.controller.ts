13d3d17566b915526c2779f8247b45bcd2103de2
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-05 22:28:14 +0700

    add local auth guard

--- src/server/auth/auth.controller.ts
+++ src/server/auth/auth.controller.ts
@@ -1,18 +1,13 @@
 import {Controller, Post, Request, UseGuards} from '@nestjs/common';
 import {Request as Req} from 'express';
-import {AuthGuard} from '@nestjs/passport';
-// import {LocalAuthGuard} from './guard/local-auth.guard'
-// import {User} from '../users/users.model';
-// import {AuthService} from './auth.service';
-// import {LoginAttrs} from './auth.types'
+import {LocalAuthGuard} from './guard/local-auth.guard'
 
 
 @Controller('auth')
 export class AuthController {
-    // constructor(@Inject(AuthService) private readonly authService: AuthService) {}
 
     @Post('login')
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     public async login(@Request() req: Req) {
         return req.user;
     }
