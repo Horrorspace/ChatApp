9bc777f26c5f10d4e704bcbf662353312875afd0
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-06 21:48:19 +0700

    edit service

--- src/server/auth/auth.service.ts
+++ src/server/auth/auth.service.ts
@@ -1,27 +1,25 @@
 import {Injectable, Inject} from '@nestjs/common';
 import bcrypt from 'bcrypt';
+import {UserAttrs} from 'server/users/users.types';
 import {UsersService} from '../users/users.service';
-import {LoginAttrs, UserAuthData} from './auth.types';
+import {LoginAttrs} from './auth.types';
 
 
 @Injectable()
 export class AuthService {
     constructor(@Inject(UsersService) private readonly usersService: UsersService) {}
 
-    public async validateUser({email, password}: LoginAttrs): Promise<UserAuthData | null> {
+    public async validateUser({email, password}: LoginAttrs): Promise<UserAttrs | null> {
         const user = await this.usersService.getUserByEmail(email);
-        console.log(user);
 
         if(!user) {
             return null
         }
 
         const isMatch: boolean = await bcrypt.compare(password, user.password);
-        console.log(isMatch);
 
         if(isMatch) {
-            const { password, ...result } = user.get();
-            return result;
+            return user.get();
         }
         else {
             return null
