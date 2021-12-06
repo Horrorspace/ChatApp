2eb2aa62e801cc640501e39ca315366d0bd1029b
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-06 21:49:24 +0700

    add updatedAt and createdAt Excludes

--- src/server/users/user.entity.ts
+++ src/server/users/user.entity.ts
@@ -2,15 +2,19 @@ import {Exclude} from 'class-transformer';
 
 
 export class UserEntity {
-    id: number;
-    online: boolean;
-    avatarSrc: string;
-    confirmed: boolean;
-    username: string;
-    email: string;
+    id!: number;
+    online!: boolean;
+    avatarSrc!: string;
+    confirmed!: boolean;
+    username!: string;
+    email!: string;
   
     @Exclude()
-    password: string;
+    password!: string;
+    @Exclude()
+    createdAt!: Date;
+    @Exclude()
+    updatedAt!: Date;
   
     constructor(partial: Partial<UserEntity>) {
       Object.assign(this, partial);
