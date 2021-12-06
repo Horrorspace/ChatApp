ce711dbbd1cd64399f5ab0565a3f7b01c36661f0
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-06 12:39:12 +0700

    Update user.serialize.ts

--- src/server/auth/serialize/user.serialize.ts
+++ src/server/auth/serialize/user.serialize.ts
@@ -1,1 +1,22 @@
+import {Injectable, Inject} from '@nestjs/common';
 
+
+@Injectable()
+export class LocalSerializer extends PassportSerializer {
+  constructor(
+    @Inject(AuthService) private readonly authService: AuthService,
+    @Inject(UserRepository) private readonly userRepository: UserRepository,
+  ) {
+    super();
+  }
+
+  serializeUser(user: User, done: CallableFunction) {
+    done(null, user.id);
+  }
+
+  async deserializeUser(userId: string, done: CallableFunction) {
+    return await this.userRepository.findOneOrFail({ id: Number(userId) })
+      .then(user => done(null, user))
+      .catch(error => done(error));
+  }
+}
