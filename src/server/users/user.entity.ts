0dd5dd13bf2bfec8371c570cd3fc02e797bf3800
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-06 07:53:39 +0700

    Update user.entity.ts

--- src/server/users/user.entity.ts
+++ src/server/users/user.entity.ts
@@ -1,1 +1,18 @@
+import {Exclude} from 'class-transformer';
 
+
+export class UserEntity {
+    id: number;
+    online: boolean;
+    avatarSrc: string;
+    confirmed: boolean;
+    username: string;
+    email: string;
+  
+    @Exclude()
+    password: string;
+  
+    constructor(partial: Partial<UserEntity>) {
+      Object.assign(this, partial);
+    }
+}
