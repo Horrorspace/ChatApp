a64b24fea0847242f845df8af0507793065fe1a2
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-06 08:12:07 +0700

    Update main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from "@nestjs/core";
+import session from 'express-session';
+import {Sequelize} from 'sequelize';
+import connectSequelize from 'connect-session-sequelize';
 import {AppModule} from './app.module';
 
 
@@ -7,6 +10,37 @@ export async function start() {
 
   const PORT: number = Number(process.env.PORT) || 3007;
   const app = await NestFactory.create(AppModule);
+  
+  const SequelizeStore = connectSequelize(session.Store);
+  const sequelize = new Sequelize({
+      dialect: 'postgres',
+      host: 'localhost',
+      database: 'sessions',
+      username: 'chat_admin',
+      password: 'KQoEgwBi',
+      logging: false
+  });
+  const sessionStore = new SequelizeStore({
+      db: sequelize,
+      checkExpirationInterval: 60*60*1000,
+      expiration: 24*60*60*1000
+  });
+  app.use(session({
+      secret: "cats and dogs",
+      store: sessionStore,
+      resave: false,
+      proxy: false,
+      saveUninitialized: false,
+      cookie: {
+          maxAge: 24*60*60*1000,
+          secure: false,
+          httpOnly: false,
+          sameSite: true,
+      }
+
+  }));
+  sessionStore.sync();
+  
 
   await app.listen(PORT);
 
