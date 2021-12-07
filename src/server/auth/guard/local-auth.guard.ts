3947f89b81085044017e4ed14579876549bf678f
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-07 08:21:50 +0700

    Update local-auth.guard.ts

--- src/server/auth/guard/local-auth.guard.ts
+++ src/server/auth/guard/local-auth.guard.ts
@@ -1,6 +1,12 @@
-import {Injectable, CanActivate} from '@nestjs/common';
+import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport';
 
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {}
+export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const result = (await super.canActivate(context)) as boolean;
+        await super.logIn(context.switchToHttp().getRequest());
+        return result;
+    }
+}
