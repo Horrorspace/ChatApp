8e6058aeb726171c9ccbbe9ac743dfd25aa00877
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-08 21:49:45 +0700

    create session module

--- src/server/sessions/sessions.module.ts
+++ src/server/sessions/sessions.module.ts
@@ -1,30 +1,55 @@
-// import {Module} from '@nestjs/common';
-// import {SequelizeModule} from "@nestjs/sequelize";
-// import {sessions} from './session.constants'
+import {Module, NestModule, MiddlewareConsumer} from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
+import {Sequelize} from 'sequelize';
+import connectSequelize from 'connect-session-sequelize';
 
-export const test = 'test';
 
+@Module({
+    controllers: [],
+    providers: [],
+    imports: [],
+    exports: []
+})
+export class SessionsModule implements NestModule {
+    private readonly SequelizeStore = connectSequelize(session.Store);
+    private readonly sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.SESSION_DB,
+        logging: false
+    });
+    private readonly sessionStore = new this.SequelizeStore({
+        db: this.sequelize,
+        checkExpirationInterval: 60*60*1000,
+        expiration: 24*60*60*1000
+    });
 
-// @Module({
-//     controllers: [],
-//     providers: [
-//         {
-//             provide: sessions,
-//             useClass: SequelizeModule.forRoot({
-//                 dialect: 'postgres',
-//                 host: process.env.POSTGRES_HOST,
-//                 port: Number(process.env.POSTGRES_PORT),
-//                 username: process.env.POSTGRES_USER,
-//                 password: process.env.POSTGRES_PASSWORD,
-//                 database: process.env.POSTGRES_DB,
-//                 models: [User, Message],
-//                 autoLoadModels: true
-//             })
-//         }
-//     ],
-//     imports: [
     
-//     ],
-//     exports: []
-// })
-// export class UsersModule {}
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                cookieParser(),
+                session({
+                    secret: "cats and dogs",
+                    store: this.sessionStore,
+                    resave: false,
+                    proxy: false,
+                    saveUninitialized: false,
+                    cookie: {
+                        maxAge: 24*60*60*1000,
+                        secure: false,
+                        httpOnly: false,
+                        sameSite: true,
+                    }
+                }), 
+                passport.initialize(),
+                passport.session(),
+            )
+            .forRoutes('*');
+    }
+}
