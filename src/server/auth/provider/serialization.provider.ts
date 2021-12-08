426f58bb9a415013671b63009772dcfa02f28e43
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-08 21:45:49 +0700

    rename

--- src/server/auth/provider/serialization.provider.ts
+++ src/server/auth/provider/serialization.provider.ts
@@ -1,7 +1,7 @@
 import {Injectable, Inject, UnauthorizedException} from '@nestjs/common';
 import {PassportSerializer} from '@nestjs/passport';
-import {User} from '../users/users.model';
-import {UsersService} from '../users/users.service';
+import {User} from '../../users/users.model';
+import {UsersService} from '../../users/users.service';
 
 @Injectable()
 export class LocalSerializer extends PassportSerializer {
@@ -12,14 +12,13 @@ export class LocalSerializer extends PassportSerializer {
     }
 
     public serializeUser(user: User, done: CallableFunction) {
-        console.log('1');
         done(null, user.id);
     }
 
     public async deserializeUser(id: number, done: CallableFunction) {
       try {
-          console.log('2');
           const user = await this.usersService.getUserById(id);
+
 
           if(user) {
               return done(null, user);
