3e8cfb66ace8fb3873b796f0abc4eb08968e12c3
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-10 22:51:06 +0700

    edit guards

--- src/server/users/users.resolver.ts
+++ src/server/users/users.resolver.ts
@@ -1,16 +1,15 @@
-import {Inject} from '@nestjs/common';
+import {Inject, UseGuards} from '@nestjs/common';
 import {Resolver, Query, Args, Mutation} from '@nestjs/graphql';
 import {UsersService} from './users.service';
-import {LoggedInGuard} from '../auth/guard/logged-in.guard';
-import {LocalAuthGuard} from '../auth/guard/local.guard';
-import {CheckIdGuard} from './guard/check-id.guard';
+import {GqlAuthGuard} from '../graphql/graphql-auth.guard';
+// import {UsersGateway} from './users.gateway';
+// import {CheckIdGuard} from './guard/check-id.guard';
 import {
     editUserNameOpt, 
     editUserEmailOpt, 
     editUserPasswordOpt, 
     editUserOnlineOpt, 
-    editUserAvatarOpt,
-    UserResponse
+    editUserAvatarOpt
 } from './users.types';
 
 
@@ -19,56 +18,50 @@ export class UsersResolver {
     constructor(@Inject(UsersService) private readonly usersService: UsersService) {}
 
     @Query()
-    @UseGuards(LoggedInGuard)
+    @UseGuards(GqlAuthGuard)
     public async users() {
         return this.usersService.getAllUsers();
     }
 
     @Query()
-    @UseGuards(LoggedInGuard)
+    @UseGuards(GqlAuthGuard)
     public async user(@Args('id') id: number) {
         return this.usersService.getUserById(id);
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
-    @UseGuards(CheckIdGuard)
-    public async editUserName(@Args({options: EditUserName}) options: editUserNameOpt) {
-        await this.usersService.editUserName(options);
+    @UseGuards(GqlAuthGuard)
+    public async editUserName(@Args('options') options: editUserNameOpt) {
+        return await this.usersService.editUserName(options);
     }
     
     @Mutation()
-    @UseGuards(LocalAuthGuard)
-    @UseGuards(CheckIdGuard)
-    public async editUserEmail(@Args({options: EditUserEmail}) options: editUserEmailOpt) {
-        await this.usersService.editUserEmail(options);
+    @UseGuards(GqlAuthGuard)
+    public async editUserEmail(@Args('options') options: editUserEmailOpt) {
+        return await this.usersService.editUserEmail(options);
     }
     
     @Mutation()
-    @UseGuards(LocalAuthGuard)
-    @UseGuards(CheckIdGuard)
-    public async editUserPassword(@Args({options: EditUserPassword}) options: editUserPasswordOpt) {
-        await this.usersService.editUserPassword(options);
+    @UseGuards(GqlAuthGuard)
+    public async editUserPassword(@Args('options') options: editUserPasswordOpt) {
+        return await this.usersService.editUserPassword(options);
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
-    @UseGuards(CheckIdGuard)
-    public async editUserOnline(@Args({options: EditUserOnline}) options: editUserOnlineOpt) {
-        await this.usersService.editUserOnline(options);
+    @UseGuards(GqlAuthGuard)
+    public async editUserOnline(@Args('options') options: editUserOnlineOpt) {
+        return await this.usersService.editUserOnline(options);
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
-    @UseGuards(CheckIdGuard)
-    public async editUserAvatar(@Args({options: EditUserAvatar}) options: editUserAvatarOpt) {
-        await this.usersService.editUserAvatar(options);
+    @UseGuards(GqlAuthGuard)
+    public async editUserAvatar(@Args('options') options: editUserAvatarOpt) {
+        return await this.usersService.editUserAvatar(options);
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
-    @UseGuards(CheckIdGuard)
+    @UseGuards(GqlAuthGuard)
     public async editUserConfirm(@Args('id') id: number) {
-        await this.usersService.confirmUser(id);
+        return await this.usersService.confirmUser(id);
     }
 }
