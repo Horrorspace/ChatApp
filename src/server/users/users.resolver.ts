c9507e38380ade04c3b2f024dbe4b05f69068f37
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-10 07:58:23 +0700

    add guard

--- src/server/users/users.resolver.ts
+++ src/server/users/users.resolver.ts
@@ -1,17 +1,22 @@
 import {Inject} from '@nestjs/common';
 import {Resolver, Query, Args} from '@nestjs/graphql';
 import {UsersService} from './users.service';
+import {LoggedInGuard} from '../auth/guard/logged-in.guard';
+//import {CheckIdGuard} from './guard/check-id.guard';
+
 
 @Resolver('User')
 export class UsersResolver {
     constructor(@Inject(UsersService) private readonly usersService: UsersService) {}
 
     @Query()
+    @UseGuards(LoggedInGuard)
     public async users() {
         return this.usersService.getAllUsers();
     }
 
     @Query()
+    @UseGuards(LoggedInGuard)
     public async user(@Args('id') id: number) {
         return this.usersService.getUserById(id);
     }
