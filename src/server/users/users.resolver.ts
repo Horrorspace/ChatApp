dbdf8c0f10b2b4f6c2bff24a4f279348c1d074da
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-10 12:43:17 +0700

    Update users.resolver.ts

--- src/server/users/users.resolver.ts
+++ src/server/users/users.resolver.ts
@@ -2,6 +2,7 @@ import {Inject} from '@nestjs/common';
 import {Resolver, Query, Args, Mutation} from '@nestjs/graphql';
 import {UsersService} from './users.service';
 import {LoggedInGuard} from '../auth/guard/logged-in.guard';
+import {LocalAuthGuard} from '../auth/guard/local.guard';
 import {CheckIdGuard} from './guard/check-id.guard';
 import {
     editUserNameOpt, 
@@ -37,16 +38,37 @@ export class UsersResolver {
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
+    @UseGuards(LocalAuthGuard)
     @UseGuards(CheckIdGuard)
     public async editUserEmail(@Args({options: EditUserEmail}) options: editUserEmailOpt) {
         await this.usersService.editUserEmail(options);
     }
     
     @Mutation()
-    @UseGuards(LoggedInGuard)
+    @UseGuards(LocalAuthGuard)
     @UseGuards(CheckIdGuard)
     public async editUserPassword(@Args({options: EditUserPassword}) options: editUserPasswordOpt) {
         await this.usersService.editUserPassword(options);
+    }
+    
+    @Mutation()
+    @UseGuards(LoggedInGuard)
+    @UseGuards(CheckIdGuard)
+    public async editUserOnline(@Args({options: EditUserOnline}) options: editUserOnlineOpt) {
+        await this.usersService.editUserOnline(options);
+    }
+    
+    @Mutation()
+    @UseGuards(LoggedInGuard)
+    @UseGuards(CheckIdGuard)
+    public async editUserAvatar(@Args({options: EditUserAvatar}) options: editUserAvatarOpt) {
+        await this.usersService.editUserAvatar(options);
+    }
+    
+    @Mutation()
+    @UseGuards(LoggedInGuard)
+    @UseGuards(CheckIdGuard)
+    public async editUserConfirm(@Args('id') id: number) {
+        await this.usersService.confirmUser(id);
     }
 }
