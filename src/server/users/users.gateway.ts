422fc9084ecfbbc8f39298f97956312ecf5725fe
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-10 12:58:44 +0700

    Update users.gateway.ts

--- src/server/users/users.gateway.ts
+++ src/server/users/users.gateway.ts
@@ -1,1 +1,24 @@
+import {
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
+
+enum socketEvents {
+    setOnline = 'set_online'
+}
+
+
+@WebSocketGateway()
+export class UsersGateway {
+    @WebSocketServer()
+    private readonly server: Server;
+  
+    @SubscribeMessage(socketEvents.setOnline)
+    public handleSetOnline(@MessageBody() online: boolean) {
+        console.log(online);
+    }
+}
