a12d31d4f1fb3a9ea4c08993ab67aabf958a5fa1
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-11 22:58:17 +0700

    init swagger

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,55 +1,26 @@
-import { NestFactory } from "@nestjs/core";
-// import session from 'express-session';
-// import {Sequelize} from 'sequelize';
-// import connectSequelize from 'connect-session-sequelize';
-// import cookieParser from 'cookie-parser';
-// import passport from 'passport'
+import {NestFactory} from "@nestjs/core";
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 import {AppModule} from './app.module';
 
 
 export async function start() {
-  console.log(`mode is: ${process.env.NODE_ENV}`);
+    console.log(`mode is: ${process.env.NODE_ENV}`);
 
-  const PORT: number = Number(process.env.PORT) || 3007;
-  const app = await NestFactory.create(AppModule);
-  
-//   const SequelizeStore = connectSequelize(session.Store);
-//   const sequelize = new Sequelize({
-//       dialect: 'postgres',
-//       host: 'localhost',
-//       database: 'chat_sessions',
-//       username: 'chat_admin',
-//       password: 'KQoEgwBi',
-//       logging: false
-//   });
-//   const sessionStore = new SequelizeStore({
-//       db: sequelize,
-//       checkExpirationInterval: 60*60*1000,
-//       expiration: 24*60*60*1000
-//   });
-//   app.use(session({
-//       secret: "cats and dogs",
-//       store: sessionStore,
-//       resave: false,
-//       proxy: false,
-//       saveUninitialized: false,
-//       cookie: {
-//           maxAge: 24*60*60*1000,
-//           secure: false,
-//           httpOnly: false,
-//           sameSite: true,
-//       }
+    const PORT: number = Number(process.env.PORT) || 3007;
+    const app = await NestFactory.create(AppModule);
 
-//   }));
-//   sessionStore.sync();
-//   app.use(cookieParser());
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-  
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('ChatApp')
+        .setDescription('ChatApp REST API description')
+        .setVersion('1.0.0')
+        .addTag('Horrorspace')
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(PORT);
-
-  console.log(`HTTP server has been started on port ${PORT}...`);
+    
+    await app.listen(PORT);
+    console.log(`HTTP server has been started on port ${PORT}...`);
 }
 
 start();
