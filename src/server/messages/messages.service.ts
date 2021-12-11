7aa13dc73f6bf04fd2db1098c80dd15e2696c50d
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-11 10:27:14 +0700

    created messages.service

--- src/server/messages/messages.service.ts
+++ src/server/messages/messages.service.ts
@@ -1,5 +1,44 @@
 import {Injectable} from '@nestjs/common';
+import {InjectModel} from '@nestjs/sequelize';
+import {Message} from './messages.model';
+import {MessageCreationAttrs} from './messages.types';
+import {Op} from 'sequelize';
 
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+    constructor(@InjectModel(Message) private readonly messageRepository: typeof Message) {}
+
+    public async createMessage(attrs: MessageCreationAttrs): Promise<Message> {
+        const message = await this.messageRepository.create(attrs);
+        return message;
+    }
+
+    public async getMessageById(id: number): Promise<Message | null> {
+        const message = await this.messageRepository.findOne({where: {id}});
+        return message;
+    }
+
+    public async getUserMessages(userId: number): Promise<Message[] | null> {
+        const messages = await this.messageRepository.findAll({
+            where: {
+                [Op.or]: [
+                    {fromUserId: userId},
+                    {toUserId: userId}
+                ]
+            }
+        });
+        return messages;
+    }
+
+    public async setReadMessage(id: number): Promise<Message | null> {
+        await this.messageRepository.update({readed: true}, {where: {id}});
+        const message = await this.getMessageById(id);
+        return message;
+    }
+
+    public async deleteMessage(id: number): Promise<null> {
+        await this.messageRepository.destroy({where: {id}});
+        return null;
+    }
+}
