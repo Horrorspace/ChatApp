01cc0f3679f52348f683094900417e52428d1c9a
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-11 19:05:51 +0700

    HttpException have been added

--- src/server/users/users.controller.ts
+++ src/server/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
     UseInterceptors, 
     ClassSerializerInterceptor, 
     UseGuards,
+    HttpException,
+    HttpStatus
 } from '@nestjs/common';
 import {UsersService} from './users.service';
 import {
@@ -16,11 +18,9 @@ import {
     editUserPasswordOpt, 
     editUserOnlineOpt, 
     editUserAvatarOpt,
-    UserResponse
 } from './users.types';
 import {UserEntity} from './user.entity';
 import {LoggedInGuard} from '../auth/guard/logged-in.guard';
-import {User} from './users.model';
 import {LocalAuthGuard} from '../auth/guard/local.guard';
 import {CheckIdGuard} from './guard/check-id.guard';
 
@@ -28,6 +28,12 @@ import {CheckIdGuard} from './guard/check-id.guard';
 @Controller('api/users')
 export class UsersController {
     constructor(@Inject(UsersService) private readonly usersService: UsersService) {}
+
+    private readonly notFoundException: HttpException = new HttpException({
+        status: HttpStatus.NOT_FOUND,
+        error: 'User not found'
+    }, HttpStatus.NOT_FOUND);
+
    
     @Get()
     @UseGuards(LoggedInGuard)
@@ -42,26 +48,26 @@ export class UsersController {
     @Get('byId')
     @UseGuards(LoggedInGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    public async getUserById(@Body('id') id: number): Promise<UserResponse | null> {
+    public async getUserById(@Body('id') id: number): Promise<UserEntity> {
         const user = await this.usersService.getUserById(id);
         if(user) {
             return new UserEntity(user.get());
         }
         else {
-            return null;
+            throw this.notFoundException
         }
     }
 
     @Get('byEmail')
     @UseGuards(LoggedInGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    public async getUserByEmail(@Body('email') email: string): Promise<UserResponse | null> {
+    public async getUserByEmail(@Body('email') email: string): Promise<UserEntity> {
         const user = await this.usersService.getUserByEmail(email);
         if(user) {
             return new UserEntity(user.get());
         }
         else {
-            return null;
+            throw this.notFoundException;
         }
     }
 
@@ -80,56 +86,111 @@ export class UsersController {
     @Put('editUserName')
     @UseGuards(LoggedInGuard)
     @UseGuards(CheckIdGuard)
-    public async editUserName(@Body() options: editUserNameOpt): Promise<User | null> {
-        await this.usersService.editUserName(options);
-        return await this.usersService.getUserById(options.id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async editUserName(@Body() options: editUserNameOpt): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(options.id);
+
+        if(isUserExist) {
+            const user = await this.usersService.editUserName(options);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Put('editUserEmail')
     @UseGuards(LocalAuthGuard)
     @UseGuards(CheckIdGuard)
-    public async editUserEmail(@Body() options: editUserEmailOpt): Promise<User | null> {
-        await this.usersService.editUserEmail(options);
-        return await this.usersService.getUserById(options.id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async editUserEmail(@Body() options: editUserEmailOpt): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(options.id);
+
+        if(isUserExist) {
+            const user = await this.usersService.editUserEmail(options);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Put('editUserPassword')
     @UseGuards(LocalAuthGuard)
     @UseGuards(CheckIdGuard)
-    public async editUserPassword(@Body() options: editUserPasswordOpt): Promise<User | null> {
-        await this.usersService.editUserPassword(options);
-        return await this.usersService.getUserById(options.id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async editUserPassword(@Body() options: editUserPasswordOpt): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(options.id);
+
+        if(isUserExist) {
+            const user = await this.usersService.editUserPassword(options);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Put('editUserOnline')
     @UseGuards(LoggedInGuard)
     @UseGuards(CheckIdGuard)
-    public async editUserOnline(@Body() options: editUserOnlineOpt): Promise<User | null> {
-        await this.usersService.editUserOnline(options);
-        return await this.usersService.getUserById(options.id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async editUserOnline(@Body() options: editUserOnlineOpt): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(options.id);
+
+        if(isUserExist) {
+            const user = await this.usersService.editUserOnline(options);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Put('editUserAvatar')
     @UseGuards(LoggedInGuard)
     @UseGuards(CheckIdGuard)
-    public async editUserAvatar(@Body() options: editUserAvatarOpt): Promise<User | null> {
-        await this.usersService.editUserAvatar(options);
-        return await this.usersService.getUserById(options.id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async editUserAvatar(@Body() options: editUserAvatarOpt): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(options.id);
+
+        if(isUserExist) {
+            const user = await this.usersService.editUserAvatar(options);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Put('confirm')
     @UseGuards(LoggedInGuard)
     @UseGuards(CheckIdGuard)
-    public async confirmUser(@Body('id') id: number): Promise<User | null> {
-        await this.usersService.confirmUser(id);
-        return await this.usersService.getUserById(id);
+    @UseInterceptors(ClassSerializerInterceptor)
+    public async confirmUser(@Body('id') id: number): Promise<UserEntity> {
+        const isUserExist: boolean = await this.checkUserId(id);
+
+        if(isUserExist) {
+            const user = await this.usersService.confirmUser(id);
+            return new UserEntity(user!.get());
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 
     @Delete()
     @UseGuards(LoggedInGuard)
     @UseGuards(CheckIdGuard)
-    public async deleteUser(@Body('id') id: number): Promise<User | null> {
-        await this.usersService.deleteUser(id);
-        return null;
+    public async deleteUser(@Body('id') id: number): Promise<null> {
+        const isUserExist: boolean = await this.checkUserId(id);
+
+        if(isUserExist) {
+            await this.usersService.deleteUser(id);
+            return null;
+        }
+        else {
+            throw this.notFoundException;
+        }
     }
 }
