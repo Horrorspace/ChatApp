8d00a59f72c6dec097652ed732dd2ce679b0b394
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-11 19:07:00 +0700

    added UserModule and SequelizeModule imports

--- src/server/messages/messages.module.ts
+++ src/server/messages/messages.module.ts
@@ -1,10 +1,18 @@
 import {Module} from '@nestjs/common';
+import {SequelizeModule} from "@nestjs/sequelize";
 import {MessagesController} from './messages.controller';
 import {MessagesService} from './messages.service';
+import {Message} from './messages.model';
+import {UsersModule} from '../users/users.module';
+
 
 @Module({
     controllers: [MessagesController],
     providers: [MessagesService],
+    imports: [
+        SequelizeModule.forFeature([Message]),
+        UsersModule
+    ],
     exports: [MessagesModule]
 })
 export class MessagesModule {}
