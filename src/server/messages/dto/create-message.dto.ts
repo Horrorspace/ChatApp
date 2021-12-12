9b494dcb7ab8692273d70ea19d8b8de4a3daa7c1
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-12 22:19:30 +0700

    created message-create dto

--- src/server/messages/dto/create-message.dto.ts
+++ src/server/messages/dto/create-message.dto.ts
@@ -1,8 +1,14 @@
 import {MessageCreationAttrs} from '../messages.types';
+import {ApiProperty} from '@nestjs/swagger';
+
 
 export class  CreateMessageDto implements MessageCreationAttrs {
     public readonly fromUserId: number;
+    
+    @ApiProperty({description: 'Уникальный идентификатор получателя'})
     public readonly toUserId: number;
+
+    @ApiProperty({description: 'Текст сообщения'})
     public readonly text: string;
 
     constructor(fromUserId: number, toUserId: number, text: string) {
