4108d83664a1d182ac5c905efc0a381ba9141d96
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-12 19:57:58 +0700

    added ApiProperty

--- src/server/users/user.entity.ts
+++ src/server/users/user.entity.ts
@@ -1,13 +1,26 @@
 import {Exclude} from 'class-transformer';
+import {ApiProperty} from '@nestjs/swagger';
 
 
 export class UserEntity {
+    @ApiProperty()
     id!: number;
+
+    @ApiProperty()
     online!: boolean;
+
+    @ApiProperty()
     avatarSrc!: string;
+
+    @ApiProperty()
     confirmed!: boolean;
+
+    @ApiProperty()
     username!: string;
+
+    @ApiProperty()
     email!: string;
+
   
     @Exclude()
     password!: string;
