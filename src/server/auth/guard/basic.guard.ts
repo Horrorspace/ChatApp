497f36749d610edd65d2da389ea437392d84213f
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-14 12:17:47 +0700

    Update basic.guard.ts

--- src/server/auth/guard/basic.guard.ts
+++ src/server/auth/guard/basic.guard.ts
@@ -6,7 +6,6 @@ import {AuthGuard} from '@nestjs/passport';
 export class BasicAuthGuard extends AuthGuard('basic') implements CanActivate {
     public async canActivate(context: ExecutionContext): Promise<boolean> {
         const result = (await super.canActivate(context)) as boolean;
-        await super.logIn(context.switchToHttp().getRequest());
         return result;
     }
 }
