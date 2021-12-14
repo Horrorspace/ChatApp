8c43056b73e59abb054e800aa7e7bf3495f36b0c
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-14 08:15:59 +0700

    created basic strategy

---
+++ src/server/auth/strategy/basic.strategy.ts
@@ -0,0 +1,28 @@
+import {PassportStrategy} from '@nestjs/passport';
+import {BasicStrategy as Strategy} from 'passport-http';
+import {Injectable, Inject, UnauthorizedException} from '@nestjs/common';
+import {AuthService} from '../provider/auth.service';
+import {UserAuthData} from '../auth.types';
+
+
+
+@Injectable()
+export class BasicStrategy extends PassportStrategy(Strategy) {
+    constructor(@Inject(AuthService) private readonly authService: AuthService) {
+        super({
+            usernameField: 'email',
+            passwordField: 'password'
+        });
+    }
+
+    public async validate(email: string, password: string): Promise<UserAuthData> {
+        const user = await this.authService.validateUser({email, password});
+
+        if(user) {
+            return user;
+        }
+        else {
+            throw new UnauthorizedException();
+        }
+    }
+}
