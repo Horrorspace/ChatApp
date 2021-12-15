ffb3b0b0a64943600fabbd988340e05015fba583
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-15 22:06:47 +0700

    edited jwt guard

--- src/server/auth/guard/jwt.guard.ts
+++ src/server/auth/guard/jwt.guard.ts
@@ -6,6 +6,7 @@ import {AuthGuard} from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
     public async canActivate(context: ExecutionContext): Promise<boolean> {
+        console.log(context);
         const result = (await super.canActivate(context)) as boolean;
         return result;
     }
