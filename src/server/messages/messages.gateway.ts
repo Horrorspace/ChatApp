7e354c49968b18b0fbe0b2c8872461d722072c99
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-15 22:07:32 +0700

    made messaging functionality

--- src/server/messages/messages.gateway.ts
+++ src/server/messages/messages.gateway.ts
@@ -1,9 +1,8 @@
-import {UseGuards, Inject} from '@nestjs/common';
+import {Inject} from '@nestjs/common';
 import {
     MessageBody,
     ConnectedSocket,
     SubscribeMessage,
-    Socket,
     WebSocketGateway,
     WebSocketServer,
     OnGatewayConnection, 
@@ -11,34 +10,77 @@ import {
 } from '@nestjs/websockets';
 import {JwtService} from '@nestjs/jwt'
 import {Server, Socket} from 'socket.io';
-import {MessageCreationAttrsRaw} from './messages.types';
-import {LoggedInGuard} from '../auth/guard/logged-in.guard';
-import {UserAttrs} from '../users/users.types';
-import {JwtAuthGuard} from '../auth/guard/jwt.guard';
-import { AccessToken } from '../auth/auth.types';
+import {MessageCreationAttrsRaw, wsClient} from './messages.types';
+// import {JwtAuthGuard} from '../auth/guard/jwt.guard';
+import {AccessToken, JwtPayload} from '../auth/auth.types';
+import {WsClientDto} from './dto/ws-client.dto';
+import {CreateMessageDto} from './dto/create-message.dto';
+import {MessagesService} from '../messages/messages.service';
+
 
 
 @WebSocketGateway()
 export class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    constructor(@Inject(JwtService) private readonly jwtService: JwtService) {}
+    constructor(
+        @Inject(JwtService) private readonly jwtService: JwtService,
+        @Inject(MessagesService) private readonly messagesService: MessagesService
+    ) {}
 
     @WebSocketServer()
     private readonly server!: Server;
 
-    private readonly clients: UserAttrs[] = [];
+    private clients: wsClient[] = [];
+
+    private getIdBySocket(client: Socket): number | null {
+        const filteredArr = this.clients.filter(wsClient => wsClient.wsId === client.id);
+
+        if(filteredArr.length === 1) {
+            return filteredArr[0].id;
+        }
+        else {
+            return null;
+        }
+    }
+
+    private getSocketId(id: number): string | null {
+        const filteredArr = this.clients.filter(wsClient => wsClient.id === id);
+
+        if(filteredArr.length === 1) {
+            return filteredArr[0].wsId;
+        }
+        else {
+            return null;
+        }
+    }
   
-    @UseGuards(LoggedInGuard)
+
     @SubscribeMessage('message')
-    public handleMessage(@MessageBody() {text, toUserId}: MessageCreationAttrsRaw) {
+    public handleMessage(
+        @MessageBody() {text, toUserId}: MessageCreationAttrsRaw,
+        @ConnectedSocket() client: Socket
+    ) {
+        const id = this.getIdBySocket(client);
+        console.log(id);
+
+        if(id) {
+            const message = new CreateMessageDto(id, toUserId, text);
+            this.messagesService.createMessage(message);
+            const fromSocket = this.getSocketId(id);
+            const toSocket = this.getSocketId(toUserId);
+            fromSocket ? this.server.to(fromSocket).emit('message', message) : null;
+            toSocket ? this.server.to(toSocket).emit('message', message) : null;
+        }
+        
         console.log(text, toUserId);
     }
 
-    @UseGuards(JwtAuthGuard)
     @SubscribeMessage('auth')
     public handleAuth(@MessageBody() {access_token}: AccessToken, @ConnectedSocket() client: Socket) {
-        console.log(client)
-        //const user = this.jwtService.decode(access_token) as UserAttrs;
-        //this.clients.push(user);
+        const {sub} = this.jwtService.decode(access_token) as JwtPayload;
+        const wsClient = new WsClientDto(client.id, sub)
+        this.clients.push(wsClient);
+        console.log(wsClient);
+        console.log(this.clients);
     }
 
     public async handleConnection(client: Socket) {
@@ -49,7 +91,7 @@ export class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect
 
     public async handleDisconnect(client: Socket) {
         console.log(`Client disconnected: ${client.id}`);
-        const sockets = await this.server.fetchSockets();
-        console.log(sockets[0]);
+        this.clients = this.clients.filter(wsClient => wsClient.wsId !== client.id);
+        console.log(this.clients);
     }
 }
