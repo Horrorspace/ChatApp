0fdb377a709892799c102a89af214a3d53a50fca
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-16 21:06:21 +0700

    added messages Query

--- src/server/messages/messages.resolver.ts
+++ src/server/messages/messages.resolver.ts
@@ -1,25 +1,37 @@
 import {Inject, UseGuards, UseInterceptors, ClassSerializerInterceptor} from '@nestjs/common';
-import {Resolver, Query, Args, Mutation} from '@nestjs/graphql';
+import {Resolver, Query, Context} from '@nestjs/graphql';
 import {MessagesService} from './messages.service';
 import {MessageEntity} from './message.entity';
 import {GqlAuthGuard} from '../graphql/graphql-auth.guard';
+import {JwtService} from '@nestjs/jwt';
+import {JwtPayload, headersObj} from '../auth/auth.types';
 
 
 
 
 @Resolver('Message')
-export class MEssagesResolver {
-    constructor(@Inject(MessagesService) private readonly messagesService: MessagesService) {}
+export class MessagesResolver {
+    constructor(
+        @Inject(MessagesService) private readonly messagesService: MessagesService,
+        @Inject(JwtService) private readonly jwtService: JwtService
+    ) {}
+
 
     @Query()
     @UseGuards(GqlAuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    public async messages(): Promise<MessageEntity[]> {
-        const user = req.user as User;
-        const userId = user.id!;
-        const messages = await this.messagesService.getUserMessages(userId);
-        return messages.map(message => {
-            return new MessageEntity(message.get())
-        });
+    public async messages(@Context() context: headersObj): Promise<MessageEntity[]> {
+        const authHeader: string | undefined = context.headers.authorization;
+        
+        if(authHeader) {
+            const {sub} = this.jwtService.decode(authHeader) as JwtPayload;
+            const messages = await this.messagesService.getUserMessages(sub);
+            return messages.map(message => {
+                return new MessageEntity(message.get())
+            });
+        }
+        else {
+            return []
+        }
     }
 }
