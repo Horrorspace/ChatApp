e2b10e8ec1ff6d2b674d7c28e4b1ea8598be492c
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-16 08:22:29 +0700

    Update graphql-auth.guard.ts

--- src/server/graphql/graphql-auth.guard.ts
+++ src/server/graphql/graphql-auth.guard.ts
@@ -1,14 +1,21 @@
-import {Injectable, ExecutionContext} from '@nestjs/common';
+import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
+import {ExecutionContextHost} from '@nestjs/core/helpers/execution-context-host;
 import {GqlExecutionContext} from '@nestjs/graphql';
 import {AuthGuard} from '@nestjs/passport';
 
 
 @Injectable()
-export class GqlAuthGuard extends AuthGuard('jwt') {
+export class GqlAuthGuard extends AuthGuard('jwt') implements CanActivate {
     public getRequest(context: ExecutionContext) {
         const ctx = GqlExecutionContext.create(context);
         console.log("gql simple context: ", context);
         console.log("gqlContext: ", ctx.getContext());
         return ctx.getContext().req;
+    }
+    
+    public async canActivate(context: ExecutionContext): Promise<boolean> {
+        const ctx = this.getRequest(context);
+        const result = (await super.canActivate(new ExecutionContextHost([ctx]))) as boolean;
+        return result;
     }
 }
