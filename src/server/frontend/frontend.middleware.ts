4747aa253c59cece018c1800cf0f82fa6d1fc56c
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-17 23:19:44 +0700

    fixed

--- src/server/frontend/frontend.middleware.ts
+++ src/server/frontend/frontend.middleware.ts
@@ -1,15 +1,17 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
+import {Injectable, NestMiddleware} from '@nestjs/common';
+import {Request, Response, NextFunction} from 'express';
 import path from 'path';
 
 
 @Injectable()
 export class FrontendMiddleware implements NestMiddleware {
-    const {url} = req;
-    if(url.indexOf('/api') === 1 || url.indexOf('/auth') === 1 || url.indexOf('/graphql') === 1) {
-        next();
-    } 
-    else {
-        res.sendFile(path.resolve(process.cwd(), 'client', 'index.html'));
+    use(req: Request, res: Response, next: NextFunction) {
+        const {url} = req;
+        if(url.indexOf('/api') === 1 || url.indexOf('/auth') === 1 || url.indexOf('/graphql') === 1) {
+            next();
+        } 
+        else {
+            res.sendFile(path.resolve(process.cwd(), 'client', 'index.html'));
+        }
     }
 }
