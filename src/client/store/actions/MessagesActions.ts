ee8701dcf36f80119a189d0114c784c8e1046ef3
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-17 08:24:16 +0700

    Update MessagesActions.ts

--- src/client/store/actions/MessagesActions.ts
+++ src/client/store/actions/MessagesActions.ts
@@ -1,1 +1,12 @@
+import {createAction} from '@reduxjs/toolkit'
+import {MessagesActTypes} from '@redux/types/MessagesActTypes';
+import {MessagesActTypes} from '@core/classes/MessageRepository';
+import {IMessage} from '@interfaces/IMessage';
 
+
+export class MessagesActions {
+    public static setMessages = createAction<IMessage[]>(MessagesActTypes.setMessages);
+    public static addMessage  = createAction<IMessage>(MessagesActTypes.addMessage);
+    public static deleteMessageFromRepository = createAction<number>(MessagesActTypes.deleteMessageFromRepository);
+    public static clearMessages  = createAction<undefined>(MessagesActTypes.clearMessages);
+}
