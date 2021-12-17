d35c5265644e372343af672768ba443cfc6dc410
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-17 13:02:48 +0700

    Update frontend.module.ts

--- src/server/frontend/frontend.module.ts
+++ src/server/frontend/frontend.module.ts
@@ -1,10 +1,11 @@
 import {Module} from '@nestjs/common';
+import {FrontendMiddleware} from './frontend.middleware';
 
 
 @Module({
     controllers: [],
-    providers: [],
+    providers: [FrontendMiddleware],
     imports: [],
-    exports: []
+    exports: [FrontendMiddleware]
 })
 export class FrontendModule {}
