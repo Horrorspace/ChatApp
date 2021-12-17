84cf66e747118733029493e7cc52a13691b35a8a
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-17 13:01:57 +0700

    Update frontend.middleware.ts

--- src/server/frontend/frontend.middleware.ts
+++ src/server/frontend/frontend.middleware.ts
@@ -1,1 +1,15 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import path from 'path';
 
+
+@Injectable()
+export class FrontendMiddleware implements NestMiddleware {
+    const {url} = req;
+    if(url.indexOf('/api') === 1 || url.indexOf('/auth') === 1 || url.indexOf('/graphql') === 1) {
+        next();
+    } 
+    else {
+        res.sendFile(path.resolve(process.cwd(), 'client', 'index.html'));
+    }
+}
