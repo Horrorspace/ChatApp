8af52a149a8ae69647850dc2f3b13267e3a99170
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-17 23:20:01 +0700

    added new actions

--- src/client/store/actions/MessagesActions.ts
+++ src/client/store/actions/MessagesActions.ts
@@ -1,12 +1,29 @@
-import {createAction} from '@reduxjs/toolkit'
-import {MessagesActTypes} from '@redux/types/MessagesActTypes';
-import {MessagesActTypes} from '@core/classes/MessageRepository';
+import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
+import {MessagesActTypes} from '@store/types/MessagesActTypes';
 import {IMessage} from '@interfaces/IMessage';
 
 
 export class MessagesActions {
+    private static getAllMessagesThunk() {}
+
+    private static sendNewMessageThunk() {}
+
+    private static setReadMessageThunk() {}
+
+    private static deleteMessageThunk() {}
+
+    
     public static setMessages = createAction<IMessage[]>(MessagesActTypes.setMessages);
-    public static addMessage  = createAction<IMessage>(MessagesActTypes.addMessage);
+
+    public static addMessage = createAction<IMessage>(MessagesActTypes.addMessage);
+
+    public static setReadMessageRepository  = createAction<number>(MessagesActTypes.setReadMessageRepository);
+
     public static deleteMessageFromRepository = createAction<number>(MessagesActTypes.deleteMessageFromRepository);
-    public static clearMessages  = createAction<undefined>(MessagesActTypes.clearMessages);
+
+    public static clearMessages = createAction<undefined>(MessagesActTypes.clearMessages);
+
+    public static getAllMessages = createAsyncThunk<undefined>(MessagesActTypes.getAllMessages, this.getAllMessagesThunk);
+
+
 }
