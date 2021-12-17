1b086c90004a70c6a03a204c665e7af82c5b0a40
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-17 08:06:29 +0700

    Update MessagesActTypes.ts

--- src/client/store/types/MessagesActTypes.ts
+++ src/client/store/types/MessagesActTypes.ts
@@ -1,1 +1,10 @@
-
+export enum MessagesActTypes {
+    setMessages = 'setMessages',
+    addMessage = 'addMessage',
+    deleteMessageFromRepository = 'deleteMessageFromRepository',
+    clearMessages = 'clearMessages',
+    getAllMessages = 'getAllMessages',
+    sendNewMessage = 'sendNewMessage',
+    setReadMessage = 'setReadMessage',
+    deleteMessage = 'deleteMessage'
+}
