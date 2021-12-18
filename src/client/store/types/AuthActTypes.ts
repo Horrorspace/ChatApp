d9cba63169da163d15233e3526ef82d8cfeb106d
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-18 12:59:26 +0700

    Update AuthActTypes.ts

--- src/client/store/types/AuthActTypes.ts
+++ src/client/store/types/AuthActTypes.ts
@@ -1,1 +1,9 @@
-
+export enum AuthActTypes {
+    setAuth = 'setAuth',
+    clearAuth = 'clearAuth',
+    setToken = 'setToken',
+    login = 'login',
+    register = 'register',
+    logout = 'logout',
+    getToken = 'getToken'
+}
