4c7040675d9a52c21a8fa748ac640af1b1377e56
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-18 08:34:51 +0700

    Update MessageReducer.ts

--- src/client/store/reducers/MessageReducer.ts
+++ src/client/store/reducers/MessageReducer.ts
@@ -1,1 +1,18 @@
+import {createAction, createReducer} from '@reduxjs/toolkit';
+import {MessageRepository} from '@core/classes/MessageRepository';
+import {MessagesActions} from '@store/actions/MessagesActions';
 
+
+
+export class MessageReducer {
+    private static readonly initialState: MessageRepository = new MessageRepository([]);
+  
+    private static buildReducer(builder) {
+        builder
+            .addCase(MessagesActions.setMessages, (state, action) => {
+                state.value = action.payload;
+            })
+    }
+  
+    public static reducer = createReducer(this.initialState, this.buildReducer)
+}
