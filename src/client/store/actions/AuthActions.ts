0f6e155077b58cf70ef1b64732b08394f309b223
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-18 13:07:30 +0700

    Update AuthActions.ts

--- src/client/store/actions/AuthActions.ts
+++ src/client/store/actions/AuthActions.ts
@@ -1,1 +1,13 @@
+import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
+import {AuthActTypes} from '@store/types/AuthActTypes';
+import {IUser} from '@interfaces/IUser';
 
+
+export class AuthActions {
+    private static loginThunkAction() {}
+
+    
+    public static setAuth = createAction<IUser>(AuthActTypes.setAuth);
+
+    public static loginThunk = createAsyncThunk<undefined>(MessagesActTypes.loginThunk, this.loginThunkAction);
+}
