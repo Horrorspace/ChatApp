653bfe2fd2324b919d8403b911a471cce704e83f
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-18 12:33:13 +0700

    Update Message.ts

--- src/client/core/classes/Message.ts
+++ src/client/core/classes/Message.ts
@@ -1,1 +1,39 @@
+import {MessageDto} from '@core/classes/message.dto';
+import {IMessage} from '@interfaces/IMessage';
 
+
+export class Message extends MessageDto {
+    constructor(
+        id: number,
+        text: string,
+        fromUserId: number,
+        toUserId: number,
+        date: Date,
+        readed: boolean
+    ) {
+        super(
+            id,
+            text,
+            fromUserId,
+            toUserId,
+            date,
+            readed
+        );
+    }
+  
+    public getMessage(): IMessage {
+        return new MessageDto(
+            this.id,
+            this.text,
+            this.fromUserId,
+            this.toUserId,
+            this.date,
+            this.readed
+        )
+    }
+  
+    public setRead(): IMessage {
+        this.readed = true;
+        return this.getMessage();
+    }
+}
