5b1ff74929adb40a73e0cb805cd8cb5f057e3b23
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-19 22:33:35 +0700

    created IAuthState

--- src/client/types/interfaces/IStore.ts
+++ src/client/types/interfaces/IStore.ts
@@ -1,4 +1,11 @@
-import {MessageRepository} from '@core/classes/MessageRepository';
+import {IToken} from '@interfaces/IAuth';
+import {IUser} from '@interfaces/IUser';
+// import {MessageRepository} from '@core/classes/MessageRepository';
 
 
 export interface IMessageState {}
+
+export interface IAuthState {
+    user: IUser | null;
+    token: IToken | null;
+}
