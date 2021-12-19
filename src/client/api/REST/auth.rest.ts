36c0052fbea197b808251e4a7bc889d3bc6235df
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-19 22:37:12 +0700

    created AuthREST

---
+++ src/client/api/REST/auth.rest.ts
@@ -0,0 +1,49 @@
+import {AbstractREST} from '@api/REST/abstract.rest';
+import {reqType} from '@api/REST/req-type.enum';
+import {ILogin, IRegister, IToken} from '@interfaces/IAuth';
+import {IUser} from '@interfaces/IUser';
+
+
+export class AuthREST extends AbstractREST {
+    private static readonly basePath: string = '/auth';
+
+    private static getUrl(path: string): string {
+        return `${AuthREST.baseUrl}${AuthREST.basePath}${path}`;
+    }
+
+    public static async login(login: ILogin): Promise<IUser> {
+        const type = reqType.post;
+        const path: string = '/login'
+        const url = AuthREST.getUrl(path);
+        return await AuthREST.makeRequest<IUser, ILogin>(type, url, login);
+    }
+
+    public static async logout(): Promise<string> {
+        const type = reqType.post;
+        const path: string = '/logout'
+        const url = AuthREST.getUrl(path);
+        return await AuthREST.makeRequest<string, void>(type, url, undefined);
+    }
+
+    public static async register(registerData: IRegister): Promise<IUser> {
+        const type = reqType.post;
+        const path: string = '/register'
+        const url = AuthREST.getUrl(path);
+        return await AuthREST.makeRequest<IUser, IRegister>(type, url, registerData);
+    }
+    
+    public static async getToken(): Promise<string> {
+        const type = reqType.post;
+        const path: string = '/getToken'
+        const url = AuthREST.getUrl(path);
+        const res = await AuthREST.makeRequest<IToken, void>(type, url, undefined);
+        return res.access_token;
+    }
+
+    public static async getUser(): Promise<IUser> {
+        const type = reqType.post;
+        const path: string = '/getUser'
+        const url = AuthREST.getUrl(path);
+        return await AuthREST.makeRequest<IUser, void>(type, url, undefined);
+    }
+}
