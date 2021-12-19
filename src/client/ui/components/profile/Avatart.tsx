9c64104ef94acdd9dfd771912cc8d7b320c716e9
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-19 22:33:15 +0700

    test api

--- src/client/ui/components/profile/Avatart.tsx
+++ src/client/ui/components/profile/Avatart.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {Row, Col, Image} from 'antd';
 import {Chat} from '@lang/en/Chat';
 import {IAvatarProps} from '@interfaces/IProps';
+import {useSelector, useDispatch} from 'react-redux';
+import {AuthActions} from '@store/actions/AuthActions';
 
 
 const Component: React.FC<IAvatarProps> = ({src}: IAvatarProps) => {
-
+    const dispatch = useDispatch();
+    const store = useSelector(state => state);
+    console.log(store);
+    
     return (
         <Row
             align="middle"
@@ -20,6 +25,10 @@ const Component: React.FC<IAvatarProps> = ({src}: IAvatarProps) => {
                     alt={Chat.avatarAlt}
                     preview={false}
                     className="profile-avatar"
+                    onClick={() => {
+                        console.log(store);
+                        dispatch(AuthActions.setToken('124215151'));
+                    }}
                 />
             </Col>
         </Row>
