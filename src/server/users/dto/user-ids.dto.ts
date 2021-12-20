dc156b0ed2007e2cf0b64efddfbe43c925441c76
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-20 13:02:50 +0700

    Update user-ids.dto.ts

--- src/server/users/dto/user-ids.dto.ts
+++ src/server/users/dto/user-ids.dto.ts
@@ -1,1 +1,12 @@
+import {UserId} from '../users.types';
+import {ApiProperty} from '@nestjs/swagger';
 
+
+export class  UserIdsDto implements UserIds {
+    @ApiProperty({description: 'Массив уникальных идентификаторов пользователей'})
+    public readonly ids: number[];
+
+    constructor(ids: number[]) {
+        this.ids = ids;
+    }
+}
