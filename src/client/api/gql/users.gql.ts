17e1685bc03aa57e72b2535a8aca32afd54c893c
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-20 13:09:40 +0700

    Update users.gql.ts

--- src/client/api/gql/users.gql.ts
+++ src/client/api/gql/users.gql.ts
@@ -1,1 +1,40 @@
+import {AbstractGql} from '@api/gql/abstract.gql';
+import {IUser} from '@interfaces/IUser';
 
+
+export class UsersGql extends AbstractGql {
+    public static async getAllUsers(): Promise<IUser> {
+        return await UsersGql.client.query({
+            query: gql`
+            query get {
+                users {
+                    id
+                    online
+                    avatarSrc
+                    confirmed
+                    username
+                    email
+                }
+            }`
+        })
+    }
+  
+    public static async getUsers(): Promise<IUser[]> {
+        return await UsersGql.client.query({
+            query: gql`
+            query get {
+                usersById({ids: [10, 11, 12]}) {
+                    id
+                    online
+                    avatarSrc
+                    confirmed
+                    username
+                    email
+                }
+            }`
+        })
+    }
+  
+  
+  
+}
