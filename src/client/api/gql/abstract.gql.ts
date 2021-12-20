71c75a01a66e4725ec148932d03f0b09a784ec07
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-20 22:28:04 +0700

    edit

--- src/client/api/gql/abstract.gql.ts
+++ src/client/api/gql/abstract.gql.ts
@@ -1,14 +1,19 @@
-import {ApolloClient, createHttpLink, InMemoryCache, gql} from '@apollo/client';
+import {ApolloClient, createHttpLink, InMemoryCache, ApolloQueryResult} from '@apollo/client';
 import {setContext} from '@apollo/client/link/context';
 import {config} from '@config/config';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+
+type gqlQuery<T, R> = (arg: T) => Promise<ApolloQueryResult<R>>
 
 
 export abstract class AbstractGql {
     protected static readonly protocol: string = config.encryption ? 'https' : 'http';
-    protected static readonly baseUrl: string = `${AbstractREST.protocol}//${config.host}:${config.port}/graphql`;
+    protected static readonly baseUrl: string = `${AbstractGql.protocol}//${config.host}:${config.port}/graphql`;
   
     protected static readonly httpLink = createHttpLink({
-        uri: abstractGql.baseUrl
+        uri: '/graphql'
     });
   
     protected static readonly authLink = setContext((_, {headers}) => {
@@ -22,7 +27,54 @@ export abstract class AbstractGql {
     });
   
     protected static readonly client = new ApolloClient({
-        link: abstractGql.authLink.concat(abstractGql.httpLink),
+        link: AbstractGql.authLink.concat(AbstractGql.httpLink),
         cache: new InMemoryCache()
     });
+
+    protected static isSuccessful<T>(apolloRes: ApolloQueryResult<T>): boolean {
+        console.log(apolloRes.errors);
+        console.log(apolloRes.error);
+        console.log(apolloRes.networkStatus);
+        
+        return !(apolloRes.errors || apolloRes.error);
+    }
+
+    protected static observableGql<T, R>(query: gqlQuery<T, R>, arg: T): Observable<ApolloQueryResult<R>> {
+        return new Observable(subscriber => {
+            query(arg)
+                .then(res => {
+                    subscriber.next(res);
+                    subscriber.complete();
+                })
+                .catch(err => {
+                    subscriber.error(err);
+                })
+        })
+    }
+
+    protected static async makeRequest<T, R>(query: gqlQuery<T, R>, arg: T): Promise<R> {
+        return new Promise((resolve, reject) => {
+            const request$ = AbstractGql.observableGql<T, R>(query, arg)
+                .pipe(
+                    map<ApolloQueryResult<R>, R>(res => {
+                        const isSuccessful = AbstractGql.isSuccessful(res);
+
+                        if(isSuccessful) {
+                            return res.data;
+                        }
+                        else {
+                            throw new Error('bad request');
+                        }
+                    })
+                );
+            request$.subscribe({
+                next: data => {
+                    resolve(data);
+                },
+                error: err => {
+                    reject(err)
+                }
+            });
+        })
+    }
 }
