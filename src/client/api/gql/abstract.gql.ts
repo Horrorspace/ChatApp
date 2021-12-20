3e96ebbfcee66560aee37b4ebf7a10077c1a3b23
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-20 12:43:23 +0700

    Update abstract.gql.ts

--- src/client/api/gql/abstract.gql.ts
+++ src/client/api/gql/abstract.gql.ts
@@ -1,1 +1,28 @@
+import {ApolloClient, createHttpLink, InMemoryCache, gql} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+import {config} from '@config/config';
 
+
+export abstract class abstractGql {
+    protected static readonly protocol: string = config.encryption ? 'https' : 'http';
+    protected static readonly baseUrl: string = `${AbstractREST.protocol}//${config.host}:${config.port}/graphql`;
+  
+    protected static readonly httpLink = createHttpLink({
+        uri: abstractGql.baseUrl
+    });
+  
+    protected static readonly authLink = setContext((_, {headers}) => {
+        const token = localStorage.getItem('token');
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : '',
+            }
+        }
+    });
+  
+    protected static readonly client = new ApolloClient({
+        link: abstractGql.authLink.concat(abstractGql.httpLink),
+        cache: new InMemoryCache()
+    });
+}
