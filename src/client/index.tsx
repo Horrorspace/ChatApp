6416398c280142bb93ce3827660915b410e04d4e
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-20 22:28:11 +0700

    test gql

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,25 +2,15 @@ import '@styles/index.less';
 import React from 'react';
 import {render} from 'react-dom';
 import {App} from '@ui/App';
-import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
+import {UsersGql} from '@api/gql/users.gql';
 
-const gqlClient = new ApolloClient({
-    uri: 'http://localhost:3007/graphql',
-    cache: new InMemoryCache()
-})
-
-
-gqlClient.query({
-    query: gql`
-    query get {
-        users {
-            id
-            confirmed
-            username
-        }
-    }`
-}).then(res => {
+UsersGql.getAllUsers().then(res => {
     console.log(res);
-})
+});
+
+UsersGql.getUsers([10, 11]).then(res => {
+    console.log(res);
+});
+
 
 render(<App />, document.getElementById('root'));
