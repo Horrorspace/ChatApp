46486e529e74e0ed8c1ead78b24db860b945dc19
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-21 08:07:54 +0700

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,33 @@ import React from 'react';
 import {render} from 'react-dom';
 import {App} from '@ui/App';
 import {UsersGql} from '@api/gql/users.gql';
+import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
+
+const gqlClient = new ApolloClient({
+    uri: 'http://localhost:3007/graphql',
+    cache: new InMemoryCache()
+})
+
+const ids_test = {
+    ids: [10, 12]
+};
+
+gqlClient.query({
+    variables: {ids_test: ids_test},
+    query: gql`
+    query get($ids_test: IdsInput) {
+        usersById(options: $ids_test) {
+            id
+            online
+            avatarSrc
+            confirmed
+            username
+            email
+        }
+    }`
+}).then(res => {
+    console.log(res);
+})
 
 UsersGql.getAllUsers().then(res => {
     console.log(res);
