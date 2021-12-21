569b0692aeed38be50ba73dadc3289b6f09c346d
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-21 12:46:20 +0700

    Update messages.socket.ts

--- src/client/api/socket/messages.socket.ts
+++ src/client/api/socket/messages.socket.ts
@@ -1,1 +1,18 @@
+import {AbstractSocket} from '@api/socket/abstract.socket';
+import {INewMessage, IMessage} from '@interfaces/IMessage';
+import {messageEventType} from '@api/socket/message-event-type.enum'
 
+
+export class MessagesSocket extends AbstractSocket {
+    private static onMessage(message: IMessage) {
+        console.log(message);
+    }
+  
+    public static sendMessage(message: INewMessage) {
+        MessagesSocket.socket.emit(messageEventType.message, message);
+    }
+  
+    public static start() {
+        MessagesSocket.socket.on(messageEventType.message, MessagesSocket.onMessage);
+    }
+}
