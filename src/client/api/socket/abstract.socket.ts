951dcf0baa6079302734b115fe3965281e7b6b1c
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-21 22:12:30 +0700

    edit abstractSocket

--- src/client/api/socket/abstract.socket.ts
+++ src/client/api/socket/abstract.socket.ts
@@ -1,8 +1,20 @@
 import {io} from 'socket.io-client';
-
-
+import {config} from '@config/config';
 
 
 export abstract class AbstractSocket {
-    protected static socket = io();
+    protected static readonly protocol: string = config.encryption ? 'wss' : 'ws';
+    protected static readonly baseUrl: string = `${AbstractSocket.protocol}//${config.host}:${config.port}`;
+    protected static token = AbstractSocket.updateToken();
+
+    protected static updateToken(): string {
+        const token = localStorage.getItem('token');
+        return token ? token : '';
+    }
+    
+    protected static socket = io(AbstractSocket.baseUrl, {
+        auth: {
+            token: AbstractSocket.token
+        }
+    });
 }
