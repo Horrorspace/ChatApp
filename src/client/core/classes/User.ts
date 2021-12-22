6079ebb1528b1e77eb4005305ba90207c18e4e2b
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-22 13:06:30 +0700

    Update User.ts

--- src/client/core/classes/User.ts
+++ src/client/core/classes/User.ts
@@ -1,1 +1,48 @@
+import {UserDto} from '@core/dto/user.dto';
+import {IUser} from '@interfaces/IUser';
 
+
+export class User extends UserDto {
+    constructor({
+        id,
+        online,
+        avatarSrc,
+        confirmed,
+        username,
+        email
+    }: IUser) {
+        super(
+            id,
+            online,
+            avatarSrc,
+            confirmed,
+            username,
+            email
+        );
+    }
+  
+    public getUser(): IUser {
+        return {
+            id: this.id,
+            online: this.online,
+            avatarSrc: this.avatarSrc,
+            confirmed: this.confirmed,
+            username: this.username,
+            email: this.email
+        }
+    }
+    
+    public getId(): number {
+        return this.id;
+    }
+  
+    public setOnline(status: boolean): IMessage {
+        this.online = status;
+        return this.getUser();
+    }
+  
+    public confirm(): IMessage {
+        this.confirm = true;
+        return this.getUser();
+    }
+}
