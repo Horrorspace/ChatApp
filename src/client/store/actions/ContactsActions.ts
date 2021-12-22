b0561bcaff5ad0c57d0c4b1e1390578431ac0139
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-22 12:53:57 +0700

    Update ContactsActions.ts

--- src/client/store/actions/ContactsActions.ts
+++ src/client/store/actions/ContactsActions.ts
@@ -1,1 +1,22 @@
+import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
+import {ContactsActTypes} from '@store/types/ContactsActTypes';
+import {IUser} from '@interfaces/IUser';
+import {UsersGql} from '@api/gql/users.gql';
 
+
+export class ContactsActions {
+    private static async  getContactsThunkAction(ids: number[]): Promise<IUser[]> {
+        return await UsersGql.getUsers(ids);
+    }
+    
+
+    public static setContacts = createAction<IUser[], MessagesActTypes>(ContactsActTypes.setContacts);
+
+    public static addContact = createAction<IUser, MessagesActTypes>(ContactsActTypes.addContact);
+
+    public static setOnline = createAction<number, MessagesActTypes>(ContactsActTypes.setOnline);
+  
+    public static deleteContact = createAction<number, MessagesActTypes>(ContactsActTypes.deleteContact);
+
+    public static getContactsThunk = createAsyncThunk<number[], IUser[]>(ContactsActTypes.getAllMessagesThunk, ContactsActions.getContactsThunkAction);
+}
