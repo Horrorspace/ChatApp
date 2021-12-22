c2cc11703b45921e31650befd54e8c35dc7c93e9
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-22 23:19:05 +0700

    fixed returning types

--- src/client/core/classes/UsersRepository.ts
+++ src/client/core/classes/UsersRepository.ts
@@ -1,4 +1,4 @@
-import {IUser} from '@interfaces/IUser';
+import {IUser, IOnline} from '@interfaces/IUser';
 import {User} from '@core/classes/User';
 
 
@@ -6,30 +6,35 @@ export class UsersRepository {
     private users: User[] = [];
     
     
-    constructor(users: IUser[] {
+    constructor(users: IUser[]) {
         this.users = users.map(user => new User(user));
     }
 
-    public getUsers(): User[] {
-        return this.users
+    public getUsers(): IUser[] {
+        return this.users.map(user => user.getUser());
     }
 
-    public addUser(user: IUser): User[] {
+    public addUser(user: IUser): IUser[] {
         const newUser = new User(user); 
         this.users.push(newUser);
         return this.getUsers();
     }
     
-    public setOnline(id: number) {
+    public setOnline({id, online}: IOnline) {
         this.users = this.users.map(user => {
             if(user.getId() === id) {
-                user.setOnline();
+                user.setOnline(online);
                 return user;
             }
             else {
                 return user;
             }
         })
+        return this.getUsers();
+    }
+
+    public deleteUser(id: number): IUser[] {
+        this.users = this.users.filter(user => user.getId() === id);
         return this.getUsers();
     }
 }
