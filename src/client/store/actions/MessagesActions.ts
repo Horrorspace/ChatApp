43a2df550fbce65c9d4f8b7485b90942b7c4c3f9
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-22 08:11:10 +0700

    Update MessagesActions.ts

--- src/client/store/actions/MessagesActions.ts
+++ src/client/store/actions/MessagesActions.ts
@@ -1,12 +1,19 @@
 import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
 import {MessagesActTypes} from '@store/types/MessagesActTypes';
-import {IMessage} from '@interfaces/IMessage';
+import {IMessage, INewMessage} from '@interfaces/IMessage';
+import {MessagesGql} from '@api/gql/messages.gql';
+import {MessagesSocket} from '@api/socket/messages.socket';
+
 
 
 export class MessagesActions {
-    private static getAllMessagesThunkAction() {}
+    private static async  getAllMessagesThunkAction(): Promise<IMessage> {
+        return await MessagesGql.getAllMessages();
+    }
 
-    private static sendNewMessageThunkAction() {}
+    private static sendNewMessageThunkAction(message: INewMessage): Promise<void> {
+        await MessagesSocket.sendMessage(message);
+    }
 
     private static setReadMessageThunkAction() {}
 
@@ -21,9 +28,9 @@ export class MessagesActions {
 
     public static deleteMessage = createAction<number, MessagesActTypes>(MessagesActTypes.deleteMessage);
 
-    public static getAllMessagesThunk = createAsyncThunk<void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
+    public static getAllMessagesThunk = createAsyncThunk<IMessage, void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
     
-    public static sendNewMessageThunk = createAsyncThunk<void>(MessagesActTypes.sendNewMessageThunk, MessagesActions.sendNewMessageThunkAction);
+    public static sendNewMessageThunk = createAsyncThunk<void, INewMessage>(MessagesActTypes.sendNewMessageThunk, MessagesActions.sendNewMessageThunkAction);
     
     public static setReadMessageThunk = createAsyncThunk<void>(MessagesActTypes.setReadMessageThunk, MessagesActions.setReadMessageThunkAction);
     
