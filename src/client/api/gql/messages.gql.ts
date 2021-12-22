aa80345e7e1cab184b23dd3d3a8ee3a6a30aff14
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-22 07:59:47 +0700

    Update messages.gql.ts

--- src/client/api/gql/messages.gql.ts
+++ src/client/api/gql/messages.gql.ts
@@ -1,1 +1,26 @@
+import {gql, ApolloQueryResult} from '@apollo/client';
+import {AbstractGql} from '@api/gql/abstract.gql';
+import {IMessage} from '@interfaces/IMessage';
 
+
+export class MessagesGql extends AbstractGql {
+    private static async getAllMessagesQuery(): Promise<ApolloQueryResult<IMessage>> {
+        return await MessagesGql.client.query({
+            query: gql`
+            query getMessages {
+                messages {
+                    id
+                    toUserId
+                    fromUserId
+                    text
+                    date
+                    readed
+                }
+            }`
+        })
+    }
+  
+    public static async getAllMessages(): Promise<IMessage> {
+        return await MessagesGql.makeRequest<void, IMessage>(MessagesGql.getAllMessagesQuery, undefined);
+    }
+}
