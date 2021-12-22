0c98c071715162077df9b18174610fb925a0eefc
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-22 12:57:07 +0700

    Update ContactsReducer.ts

--- src/client/store/reducers/ContactsReducer.ts
+++ src/client/store/reducers/ContactsReducer.ts
@@ -1,4 +1,4 @@
 import {createReducer, ActionReducerMapBuilder} from '@reduxjs/toolkit';
-import {MessageRepository} from '@core/classes/UsersRepository';
+import {UsersRepository} from '@core/classes/UsersRepository';
 import {ContactsActions} from '@store/actions/ContactsActions';
 import {IUser} from '@interfaces/IUser';
