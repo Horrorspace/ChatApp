1d5f7921fcc1334407346c54b116acd5abddf91b
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-22 23:20:07 +0700

    fixed types

--- src/client/store/actions/MessagesActions.ts
+++ src/client/store/actions/MessagesActions.ts
@@ -2,17 +2,12 @@ import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
 import {MessagesActTypes} from '@store/types/MessagesActTypes';
 import {IMessage, INewMessage} from '@interfaces/IMessage';
 import {MessagesGql} from '@api/gql/messages.gql';
-import {MessagesSocket} from '@api/socket/messages.socket';
 
 
 
 export class MessagesActions {
-    private static async  getAllMessagesThunkAction(): Promise<IMessage> {
+    private static async  getAllMessagesThunkAction(): Promise<IMessage[]> {
         return await MessagesGql.getAllMessages();
-    }
-
-    private static sendNewMessageThunkAction(message: INewMessage): Promise<void> {
-        await MessagesSocket.sendMessage(message);
     }
 
     private static setReadMessageThunkAction() {}
@@ -24,13 +19,13 @@ export class MessagesActions {
 
     public static addMessage = createAction<IMessage, MessagesActTypes>(MessagesActTypes.addMessage);
 
+    public static sendNewMessage = createAction<INewMessage, MessagesActTypes>(MessagesActTypes.sendNewMessageThunk);
+
     public static setReadMessage = createAction<number, MessagesActTypes>(MessagesActTypes.setReadMessage);
 
     public static deleteMessage = createAction<number, MessagesActTypes>(MessagesActTypes.deleteMessage);
 
-    public static getAllMessagesThunk = createAsyncThunk<IMessage, void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
-    
-    public static sendNewMessageThunk = createAsyncThunk<void, INewMessage>(MessagesActTypes.sendNewMessageThunk, MessagesActions.sendNewMessageThunkAction);
+    public static getAllMessagesThunk = createAsyncThunk<IMessage[], void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
     
     public static setReadMessageThunk = createAsyncThunk<void>(MessagesActTypes.setReadMessageThunk, MessagesActions.setReadMessageThunkAction);
     
