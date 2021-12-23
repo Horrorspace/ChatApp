882d336c7afae3d9bfd8a5de84ad89d5cd1c2224
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-23 08:32:23 +0700

    Update contact-state.dto.ts

--- src/client/core/dto/contact-state.dto.ts
+++ src/client/core/dto/contact-state.dto.ts
@@ -1,1 +1,16 @@
+import {IContactsState} from '@interfaces/IStore';
+import {IUser} from '@interfaces/IUser';
 
+
+export class ContactsStateDto implements IContactsState {
+    public contacts: IUser[];
+    public currentContact: IUser | null;
+
+    constructor(
+        contacts: IUser[],
+        currentContact: IUser | null
+    ) {
+        this.contacts = contacts;
+        this.currentContact = currentContact;
+    }
+}
