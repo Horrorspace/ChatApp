5f92b387ce3efda30b3f0eba20834c94c116437d
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-23 08:42:19 +0700

    Update ContactsRepository.ts

--- src/client/core/classes/ContactsRepository.ts
+++ src/client/core/classes/ContactsRepository.ts
@@ -1,1 +1,41 @@
+import {IContactsState} from '@interfaces/IStore';
 
+
+export class ContactsRepository {
+    public contacts: IUser[];
+    public currentContact: IUser | null;
+  
+    constructor({contacts, currentContact}: IContactsState) {
+        this.contacts = contacts;
+        this.currentContact = currentContact;
+    }
+  
+    public getState(): IContactsState {
+        return {
+            contacts: this.contacts,
+            currentContact: this.currentContact
+        }
+    }
+  
+    public getContacts(): IUser[] {
+        return this.contacts
+    }
+  
+    public getCurrentContact(): IUser {
+        return this.currentContact
+    }
+  
+    public setContacts(contacts: IUser[]): IUser[] {
+        this.contacts = contacts;
+        return this.getContacts();
+    }
+  
+    public setCurrentContact(currentContact: IUser): IUser {
+        this.currentContact = currentContact;
+        return this.getCurrentContact();
+    }
+  
+    public clearCurrentContact(): void {
+        this.currentContact = null;
+    }
+}
