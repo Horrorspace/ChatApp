d65d7175ad75bc6a4506243c1fbe816de90681ac
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-24 08:17:47 +0700

    Update ContactsReducer.ts

--- src/client/store/reducers/ContactsReducer.ts
+++ src/client/store/reducers/ContactsReducer.ts
@@ -2,21 +2,26 @@ import {createReducer, ActionReducerMapBuilder} from '@reduxjs/toolkit';
 import {UsersRepository} from '@core/classes/UsersRepository';
 import {ContactsActions} from '@store/actions/ContactsActions';
 import {IUser} from '@interfaces/IUser';
+import {IContactsState} from '@interfaces/IStore';
 import {ContactsRepository} from '@core/classes/ContactsRepository';
+import {ContactsStateDto} from '@core/dto/contact-state.dto';
+
 
 
 export class ContactsReducer {
-    private static readonly initialState: IUser[] = new UsersRepository([]).getUsers();
+    private static readonly initialState: IContactsState = new ContactsRepository(new ContactsStateDto([], null)).getState();
 
     private static buildReducer(builder: ActionReducerMapBuilder<IUser[]>): void {
         builder
             .addCase(ContactsActions.setContacts, (state, action) => {
-                return action.payload;
+                const contacts = new ContactsRepository(state);
+                contacts.setContacts(action.payload)
+                return contacts.getState();
             })
             .addCase(ContactsActions.addContact, (state, action) => {
-                const users = new UsersRepository(state);
-                users.addUser(action.payload);
-                return users.getUsers();
+                const contacts = new ContactsRepository(state);
+                contacts.addContact(action.payload)
+                return contacts.getState();
             })
             .addCase(ContactsActions.setOnline, (state, action) => {
                 const users = new UsersRepository(state);
