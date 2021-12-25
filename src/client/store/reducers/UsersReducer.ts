c920a389c8e66ffc5d97e7b787a125ffc88b9dce
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-25 09:31:26 +0700

    created UsersReducer

---
+++ src/client/store/reducers/UsersReducer.ts
@@ -0,0 +1,21 @@
+import {createReducer, ActionReducerMapBuilder} from '@reduxjs/toolkit';
+import {UsersActions} from '@store/actions/UsersActions';
+import {IUser} from '@interfaces/IUser';
+
+
+
+export class UsersReducer {
+    private static readonly initialState: IUser[] = [];
+
+    private static buildReducer(builder: ActionReducerMapBuilder<IUser[]>): void {
+        builder
+            .addCase(UsersActions.setUsers, (state, action) => {
+                return action.payload;
+            })
+            .addCase(UsersActions.getContactsThunk.fulfilled, (state, action) => {
+                return action.payload;
+            });
+    }
+
+    public static reducer = createReducer(UsersReducer.initialState, UsersReducer.buildReducer);
+}
