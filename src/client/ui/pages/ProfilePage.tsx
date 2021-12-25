08e05ffc430301dbc70e4bc10edbd922ac0b643f
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-25 09:29:07 +0700

    remove comments, add redux test

--- src/client/ui/pages/ProfilePage.tsx
+++ src/client/ui/pages/ProfilePage.tsx
@@ -1,36 +1,18 @@
 import React, {} from 'react';
+import {useSelector} from 'react-redux';
 import {Chat} from '@lang/en/Chat';
 import {Profile} from '@ui/components/profile/Profile';
 import {ProfileMenu} from '@ui/components/profile/ProfileMenu';
 import {IInfoFieldProps} from '@interfaces/IProps';
+import {IRootState} from '@interfaces/IStore';
 import {InfoFields} from '@aliases/Profile';
 // import {ILogin} from '@interfaces/IAuth';
 
 
 
 export const ProfilePage: React.FC = () => {
-    // useEffect(() => {
-    //     const dispatch = useDispatch();
-    //     const store = useSelector(state => state);
-    //     const login: ILogin = {
-    //         email: "tester3111@test.com",
-    //         password: "1111"
-    //     }
-    
-    //     console.log(store);
-    //     dispatch(AuthActions.setToken('1526236232'));
-    //     console.log(store);
-    //     dispatch(AuthActions.loginThunk(login));
-    
-    //     setTimeout(() => {
-    //         console.log(store);
-    //     }, 5000)
-    // }, [])
-
-    // useEffect(() => {
-    //     const store = useSelector(state => state);
-    //     console.log(store);
-    // }, [])
+    const store = useSelector((state: IRootState): IRootState => state);
+    console.log(store.Contacts.contacts);
 
     
     const avatarSrc: string = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album"
