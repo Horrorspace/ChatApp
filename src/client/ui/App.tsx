d602406956cf936febf1529276d6ec363df66bc5
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-25 09:32:11 +0700

    added HOCs

--- src/client/ui/App.tsx
+++ src/client/ui/App.tsx
@@ -1,40 +1,24 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {compose} from 'redux';
 // import {ChatPage} from '@ui/pages/ChatPage';
 // import {ContactsPage} from '@ui/pages/ContactsPage';
+import {withContent} from '@ui/HOCs/withContent';
+import {withProvider} from '@ui/HOCs/withProvider';
 import {ProfilePage} from '@ui/pages/ProfilePage';
 // import {AuthPage} from '@ui/pages/AuthPage';
 // import {RegisterPage} from '@ui/pages/RegisterPage';
-import {Provider} from 'react-redux';
-import {Store} from '@store/Store';
 
-
-const {Content} = Layout;
 
 export const App: React.FC = () => {
-    // useEffect(() => {
-    //     const login: ILogin = {
-    //         email: "tester3111@test.com",
-    //         password: "1111"
-    //     }
-
-    //     AuthREST.login(login)
-    //         .then(res => {
-    //             console.log(res)
-    //         });
-    // })
+    const Component = compose<React.FC>(
+        withProvider,
+        withContent
+    )(ProfilePage)
     
+   
     return (
-        <Provider store={Store.value}>
-            <Layout
-                className="app"
-            >
-                <Content
-                    className="main"
-                >
-                    <ProfilePage />
-                </Content>
-            </Layout>
-        </Provider>
+        <>
+            <Component />
+        </>
     )
 }
