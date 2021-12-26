32dbf3ebdd4a854d8497c2a197362526185fae19
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-26 11:19:19 +0700

    created Router component

---
+++ src/client/ui/Router.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useRoutes} from '@ui/hooks/useRoutes';
+
+
+export const Router: React.FC = () => {
+    const routes: React.ReactElement = useRoutes(true);    
+   
+    return (
+        <>
+            {routes}
+        </>
+    )
+}
