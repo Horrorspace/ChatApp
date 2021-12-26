1cd625d52ad16ce0624a08f408b6970dfb3eb70e
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-26 22:24:55 +0700

    added class names

--- src/client/ui/components/auth/GoogleAuth.tsx
+++ src/client/ui/components/auth/GoogleAuth.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Row, Col, Button, Typography} from 'antd';
+import {GoogleOutlined} from '@ant-design/icons';
 import {Chat} from '@lang/en/Chat';
 import {IGoogleAuthProps} from '@interfaces/IProps'
 
@@ -11,33 +12,42 @@ const Component: React.FC<IGoogleAuthProps> = ({aMember}: IGoogleAuthProps) => {
     const signUpTitle: string = `${Chat.signUp} ${Chat.withGoogle}:`;
     const title: string = aMember ? signInTitle : signUpTitle;
 
-    const signInButton: string = Chat.googleAuth;
-    const signUpButton: string = Chat.googleAuth;
-    const buttonText: string = aMember ? signInButton : signUpButton;
     
     return (
-        <>
-            <Row
-                align="middle"
-                justify="center"    
+        <section
+            className="google-auth__wrap"
+        >
+            <div
+                className="google-auth"
             >
-                <Col>
-                    <Title level={4}>
-                        {title}
-                    </Title>
-                </Col>
-            </Row>
-            <Row
-                align="middle"
-                justify="center"
-            >
-                <Col>
-                    <Button>
-                        {buttonText}
-                    </Button>
-                </Col>
-            </Row>
-        </>
+                <Row
+                    align="middle"
+                    justify="center"
+                    className="google-auth-title__wrap"
+                >
+                    <Col>
+                        <Title level={4}
+                            className="google-auth-title"
+                        >
+                            {title}
+                        </Title>
+                    </Col>
+                </Row>
+                <Row
+                    align="middle"
+                    justify="center"
+                    className="google-auth-btn__wrap"
+                >
+                    <Col>
+                        <Button
+                            className="google-auth-btn"
+                        >
+                            <GoogleOutlined />
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+        </section>
     )
 }
 
