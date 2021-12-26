32b909a48ee5dce3eaf3d3e233323f30b7f47186
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-26 22:23:22 +0700

    add NestExpressApplication

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,13 +1,28 @@
 import {NestFactory} from "@nestjs/core";
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {NestExpressApplication} from '@nestjs/platform-express';
 import {AppModule} from './app.module';
+// import {join} from 'path';
+// import express from 'express';
+// import path from 'path';
 
 
 export async function start() {
     console.log(`mode is: ${process.env.NODE_ENV}`);
 
     const PORT: number = Number(process.env.PORT) || 3007;
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    // const __dirname: string = path.dirname(__filename);
+    // app.use('/public/', express.static(path.join(__dirname, 'client', 'public')));
+
+    // console.log('app ', path.join(__dirname, 'client', 'public'));
+
+    // app.use('/public/', express.static(join(process.cwd(), 'build', 'client', 'public')));
+
+    // app.useStaticAssets(join(process.cwd(), 'build', 'client', 'public'), {
+    //     prefix: '/public/'
+    // });
 
     const swaggerConfig = new DocumentBuilder()
         .setTitle('ChatApp')
