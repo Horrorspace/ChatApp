b0cdd76749b71ba0a450c5e352c5a855e4c52da8
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-27 12:27:39 +0700

    Update ui.ts

--- src/client/types/aliases/ui.ts
+++ src/client/types/aliases/ui.ts
@@ -1,3 +1,7 @@
 import {ReactElement} from 'react';
 
 export type ElementCreator<T> = (Props: T) => ReactElement;
+
+export type inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+export type btnClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => void;
