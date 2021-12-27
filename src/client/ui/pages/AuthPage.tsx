11376a61f2e9deb426c5a98893e9ffd1bb6995e6
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-27 12:13:34 +0700

    Update AuthPage.tsx

--- src/client/ui/pages/AuthPage.tsx
+++ src/client/ui/pages/AuthPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {GoogleAuth} from '@ui/components/auth/GoogleAuth';
 import {SignIn} from '@ui/components/auth/SignIn';
 import {AMember} from '@ui/components/auth/AMember';
 
 
 export const AuthPage: React.FC = () => {
-
+    const dispatch = useDispatch();
+    
     return (
         <div
             className="auth"
