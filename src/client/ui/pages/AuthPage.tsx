3e6a5246e6dffe2573416ed73a9d83429461979a
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-27 12:52:47 +0700

    Update AuthPage.tsx

--- src/client/ui/pages/AuthPage.tsx
+++ src/client/ui/pages/AuthPage.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {GoogleAuth} from '@ui/components/auth/GoogleAuth';
 import {SignIn} from '@ui/components/auth/SignIn';
 import {AMember} from '@ui/components/auth/AMember';
+import {inputChangeHandler, btnClickHandler} from '@aliases/ui';
+
+
+interface AuthState {
+    login: string;
+    password: string;
+}
 
 
 export const AuthPage: React.FC = () => {
+    const initialState: AuthState = {
+        login: '';
+        password: '';
+    };
+    const [state, setState] = useState<AuthState>(initialState);
     const dispatch = useDispatch();
+    
+    const loginHandler: inputChangeHandler = (e) => {
+        const target = e.target as HTMLInputElement;
+        const login: string = target.value;
+        setState(prev => ({
+            ...prev,
+            login 
+        }));
+    }
+    
+    const passwordHandler: inputChangeHandler = (e) => {
+        const target = e.target as HTMLInputElement;
+        const password: string = target.value;
+        setState(prev => ({
+            ...prev,
+            password 
+        }));
+    }
+    
+    const signInHandler: btnClickHandler = (e) => {
+        setState(initialState);
+    }
+    
     
     return (
         <div
@@ -15,7 +50,13 @@ export const AuthPage: React.FC = () => {
             <GoogleAuth 
                 aMember={true}
             />
-            <SignIn />
+            <SignIn
+                login={state.login}
+                password={state.password}
+                onLoginChange={}
+                onPasswordChange={}
+                onSignInClick={}                
+            />
             <AMember 
                 aMember={true}
             />
