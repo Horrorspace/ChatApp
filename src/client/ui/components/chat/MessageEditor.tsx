868e308300f975b507662fb42b2582cad50184ac
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-12-29 13:05:54 +0700

    Update MessageEditor.tsx

--- src/client/ui/components/chat/MessageEditor.tsx
+++ src/client/ui/components/chat/MessageEditor.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {Row, Col, Button, Input} from 'antd';
 import {SendOutlined} from '@ant-design/icons';
 import {Chat} from '@lang/en/Chat';
+import {ISignInProps} from '@interfaces/IProps';
 
 
-const Component: React.FC = () => { 
+const Component: React.FC<ISignInProps> = ({onSendClicK, onTextChange}: ISignInProps) => { 
     return (
         <section
             className="message-editor__wrap_dark"
@@ -20,6 +21,7 @@ const Component: React.FC = () => {
                     <Input 
                         placeholder={Chat.messagePlaceholder}
                         className="message-input_dark"
+                        onChange={onTextChange}
                     />
                 </Col>
                 <Col
@@ -28,6 +30,7 @@ const Component: React.FC = () => {
                     <Button 
                         type="primary"
                         className="message-send-btn_dark"
+                        onClick={onSendClicK}
                     >
                         <SendOutlined />
                     </Button>
