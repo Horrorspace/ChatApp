787229ef5b0e6f3344f798c1829cf557da6f1f0c
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-12-31 00:46:49 +0700

    remove dispatch

--- src/client/ui/Router.tsx
+++ src/client/ui/Router.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
 import {useRoutes} from '@ui/hooks/useRoutes';
 import {IRootState} from '@interfaces/IStore';
 
 
 export const Router: React.FC = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     const user = useSelector<IRootState>(state => state.Auth.user);
     
     const routes: React.ReactElement = useRoutes(Boolean(user));    
