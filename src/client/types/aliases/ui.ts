3083ff945df2bc9113b244462a5870f3b14f605d
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-02 11:30:58 +0700

    add clickHandler and clickHandlerVoid

--- src/client/types/aliases/ui.ts
+++ src/client/types/aliases/ui.ts
@@ -5,3 +5,7 @@ export type ElementCreator<T> = (Props: T) => ReactElement;
 export type inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => void;
 
 export type btnClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => void;
+
+export type clickHandler<T = HTMLElement> = (e: React.MouseEvent<HTMLElement>) => void;
+
+export type clickHandlerVoid = () => void;
