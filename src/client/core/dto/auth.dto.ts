15b485a129d0fe5716d114e13dde1118212c6128
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-02 11:09:15 +0700

    add loading prop

--- src/client/core/dto/auth.dto.ts
+++ src/client/core/dto/auth.dto.ts
@@ -6,13 +6,16 @@ import {IUser} from '@interfaces/IUser';
 export class AuthDto implements IAuthState {
     public readonly user: IUser | null;
     public readonly token: IToken | null;
+    public readonly loading: boolean;
 
 
     constructor(
         user: IUser | null,
-        token: IToken | null
+        token: IToken | null,
+        loading: boolean
     ) {
         this.user = user;
         this.token = token;
+        this.loading = loading;
     }
 }
