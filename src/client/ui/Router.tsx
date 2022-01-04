199af779a6002626f970229f8a0ff030e9ed2243
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:05:00 +0700

    add userLoading and tokenLoading

--- src/client/ui/Router.tsx
+++ src/client/ui/Router.tsx
@@ -6,10 +6,10 @@ import {LoadingPage} from '@ui/pages/LoadingPage';
 
 
 export const Router: React.FC = () => {
-    const loading = useSelector<IRootState>(state => state.Auth.loading);
+    const userLoading = useSelector<IRootState, boolean>(state => state.Auth.userLoading);
+    const tokenLoading = useSelector<IRootState, boolean>(state => state.Auth.tokenLoading);
+    const loading: boolean = userLoading || tokenLoading;
     const user = useSelector<IRootState>(state => state.Auth.user);
-    console.log(loading);
-    console.log(user);
     const routes: React.ReactElement = useRoutes(Boolean(user));    
 
     if(loading) {
