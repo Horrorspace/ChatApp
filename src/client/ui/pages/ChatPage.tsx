15eb0fda9aa775ce26817fbe490ce0f270fcc19d
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 23:56:19 +0700

    edit props

--- src/client/ui/pages/ChatPage.tsx
+++ src/client/ui/pages/ChatPage.tsx
@@ -72,20 +72,18 @@ export const ChatPage: React.FC = () => {
             }));
         }
     }
-
-    const avatarSrc = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album"
-   
+  
     return (
         <>
             <Contact 
-                username="Just for lulz"
-                avatarSrc={avatarSrc}
-                online={false}
-                lastOnline={new Date('November 14, 2021 19:28:00')}
+                username={contact.username}
+                avatarSrc={contact.avatarSrc}
+                online={contact.online}
+                lastOnline={null}
             />
             <Messages 
                 messages={messages}
-                userId={10}
+                userId={contact.id}
             />
             <MessageEditor
                 text={state.text}
