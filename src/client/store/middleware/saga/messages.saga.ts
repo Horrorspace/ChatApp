3efd77812cc27b787d137c2cea6d492b403e797a
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:02:02 +0700

    create messages saga

---
+++ src/client/store/middleware/saga/messages.saga.ts
@@ -0,0 +1,35 @@
+import {Store} from '@store/Store';
+import {IRootState} from '@interfaces/IStore';
+import {MessagesActTypes} from '@store/types/MessagesActTypes';
+import {ContactsActions} from '@store/actions/ContactsActions';
+import {takeEvery, put} from 'redux-saga/effects';
+
+
+export class MessagesSaga {
+    private static *contactsSaga() {
+        const state: IRootState = Store.value.getState();
+        const user = state.Auth.user;
+        console.log(state.Messages);
+        if(user) {
+            const ids: number[] = state.Messages.map(message => {
+                if(message.fromUserId !== user.id) {
+                    return message.fromUserId
+                }
+                else {
+                    return message.toUserId
+                }
+            });
+            yield put<any>(ContactsActions.getContactsThunk(ids));
+        }
+    }
+
+    private static *addSaga() {
+        const state: IRootState = Store.value.getState();
+        console.log(state.Messages);
+    }
+
+    public static *sagaWatcher() {
+        yield takeEvery(MessagesActTypes.setMessages, MessagesSaga.contactsSaga);
+        yield takeEvery(MessagesActTypes.addMessage, MessagesSaga.addSaga);
+    }
+}
