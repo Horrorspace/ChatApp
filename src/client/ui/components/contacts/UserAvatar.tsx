b8ee15ef4b0dd4dc0f067057264e203b41d0042a
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 23:55:38 +0700

    add defAvatar

--- src/client/ui/components/contacts/UserAvatar.tsx
+++ src/client/ui/components/contacts/UserAvatar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Col, Image} from 'antd';
 import {Chat} from '@lang/en/Chat';
+import {defAvatar} from '@config/defAvatar';
 import {IUserAvatarProps} from '@interfaces/IProps';
 
 
 export const Component: React.FC<IUserAvatarProps> = ({lastMessageFromUserId, userId, userAvatarSrc}: IUserAvatarProps) => {
     const lastMessagefromUser: boolean = lastMessageFromUserId !== userId;
+    const avatar = userAvatarSrc.length !== 0 ? userAvatarSrc : defAvatar;
     
     if(lastMessagefromUser) {
         return (
@@ -13,7 +15,7 @@ export const Component: React.FC<IUserAvatarProps> = ({lastMessageFromUserId, us
                     className="contact-card-user-avatar__wrap"
                 >
                     <Image 
-                        src={userAvatarSrc}
+                        src={avatar}
                         alt={Chat.avatarAlt}
                         preview={false}
                         className="contact-card-user-avatar"
