01c2f3e8860f7d7d6fe3b73def77859875a3ba89
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:08:25 +0700

    edit page

--- src/client/ui/pages/ChatPage.tsx
+++ src/client/ui/pages/ChatPage.tsx
@@ -1,85 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {Messages} from '@ui/components/chat/Messages';
 import {Contact} from '@ui/components/chat/Contact';
 import {MessageEditor} from '@ui/components/chat/MessageEditor';
-import {IMessage} from '@interfaces/IMessage';
+import {MessagesActions} from '@store/actions/MessagesActions';
+import {NewMessageDto} from '@core/dto/new-message.dto';
+import {IRootState} from '@interfaces/IStore';
+import {IMessage, INewMessage} from '@interfaces/IMessage';
+import {IUser} from '@interfaces/IUser';
 import {inputChangeHandler, btnClickHandler} from '@aliases/ui';
 
 
+interface IChatPageState {
+    text: string;
+}
+
+function scroll() {
+    const scrollWindow = document.querySelector(".message-list-wrap");
+    if(scrollWindow) {
+        scrollWindow.scrollTo(0, scrollWindow.scrollHeight - scrollWindow.clientHeight);
+    }
+}
+
+
 export const ChatPage: React.FC = () => {
-   const messages: IMessage[] = [
-        {
-            id: 1,
-            text: 'One',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 03:20:00'),
-            readed: true
-        },
-        {
-            id: 2,
-            text: 'Two',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 09:41:00'),
-            readed: true
-        },
-        {
-            id: 3,
-            text: 'Three',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 12:18:00'),
-            readed: true
-        },
-        {
-            id: 4,
-            text: 'Four',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 16:54:00'),
-            readed: true
-        },
-        {
-            id: 5,
-            text: 'Five',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 19:28:00'),
-            readed: true
-        },
-        {
-            id: 6,
-            text: 'Six',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 19:28:00'),
-            readed: true
-        },
-        {
-            id: 7,
-            text: 'Seven \n Seven',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 19:28:00'),
-            readed: true
-        },
-        {
-            id: 8,
-            text: 'Eight \n Eight Eight \n Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight Eight',
-            fromUserId: 10,
-            toUserId: 9,
-            date: new Date('November 14, 2021 19:28:00'),
-            readed: true
-        },
-    ];
+    useEffect(() => {
+        scroll();
+    })
+
+
+    const dispatch = useDispatch();
+    const contact = useSelector<IRootState, IUser>(state => state.Contacts.currentContact!);
+    const allMessages = useSelector<IRootState, IMessage[]>(state => state.Messages);
+    const messages = allMessages
+        .filter(message => {
+            const fromUser = message.fromUserId === contact.id;
+            const toUser = message.toUserId === contact.id;
+            return fromUser || toUser;
+        })
+        .map(message => ({
+            ...message,
+            date: new Date(message.date)
+        }));
+    
+
+    const initialState: IChatPageState = {
+        text: ''
+    }
+    const [state, setState] = useState(initialState);
    
     const handleSend: btnClickHandler = () => {
-    
+        const text = state.text;
+        const toUserId = contact.id;
+        if(text.length > 0) {
+            const newMessage: INewMessage = new NewMessageDto(text, toUserId).getMessage();
+            dispatch(MessagesActions.sendNewMessage(newMessage));
+            setState(state => ({
+                ...state,
+                text: ''
+            }))
+        }
     }
     
-    const handleInput: inputChangeHandler = () => {
-    
+    const handleInput: inputChangeHandler = (e) => {
+        if(e) {
+            const target = e.target as HTMLInputElement;
+            const value = target.value;
+            setState(state => ({
+                ...state,
+                text: value
+            }));
+        }
     }
 
     const avatarSrc = "https://sun9-76.userapi.com/impg/iIWIxmXrMksl12Z47tqfYqZl_0XLIGFyf4Shng/5anoVOOFRkY.jpg?size=1192x871&quality=96&sign=9437dcf14a264d0f1eb4efa06544eb8e&type=album"
@@ -97,6 +88,7 @@ export const ChatPage: React.FC = () => {
                 userId={10}
             />
             <MessageEditor
+                text={state.text}
                 onSendClicK={handleSend}
                 onTextChange={handleInput}
             />
