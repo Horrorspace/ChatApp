5b07c6b91f4dea08860ac00cf246cccc9b53bf90
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:01:26 +0700

    edit getAllMessagesThunkAction

--- src/client/store/actions/MessagesActions.ts
+++ src/client/store/actions/MessagesActions.ts
@@ -2,12 +2,14 @@ import {createAction, createAsyncThunk} from '@reduxjs/toolkit'
 import {MessagesActTypes} from '@store/types/MessagesActTypes';
 import {IMessage, INewMessage} from '@interfaces/IMessage';
 import {MessagesGql} from '@api/gql/messages.gql';
+import {Store} from '@store/Store';
 
 
 
 export class MessagesActions {
-    private static async  getAllMessagesThunkAction(): Promise<IMessage[]> {
-        return await MessagesGql.getAllMessages();
+    private static async  getAllMessagesThunkAction(): Promise<void> {
+        const res = await MessagesGql.getAllMessages();
+        Store.value.dispatch(MessagesActions.setMessages(res.messages));
     }
 
     private static setReadMessageThunkAction() {}
@@ -19,13 +21,13 @@ export class MessagesActions {
 
     public static addMessage = createAction<IMessage, MessagesActTypes>(MessagesActTypes.addMessage);
 
-    public static sendNewMessage = createAction<INewMessage, MessagesActTypes>(MessagesActTypes.sendNewMessageThunk);
+    public static sendNewMessage = createAction<INewMessage, MessagesActTypes>(MessagesActTypes.sendNewMessage);
 
     public static setReadMessage = createAction<number, MessagesActTypes>(MessagesActTypes.setReadMessage);
 
     public static deleteMessage = createAction<number, MessagesActTypes>(MessagesActTypes.deleteMessage);
 
-    public static getAllMessagesThunk = createAsyncThunk<IMessage[], void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
+    public static getAllMessagesThunk = createAsyncThunk<void, void>(MessagesActTypes.getAllMessagesThunk, MessagesActions.getAllMessagesThunkAction);
     
     public static setReadMessageThunk = createAsyncThunk<void>(MessagesActTypes.setReadMessageThunk, MessagesActions.setReadMessageThunkAction);
     
