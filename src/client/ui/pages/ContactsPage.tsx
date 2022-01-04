ffcb36e7f7b1e8371f755b2b52f4408ac101c502
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:08:31 +0700

    edit page

--- src/client/ui/pages/ContactsPage.tsx
+++ src/client/ui/pages/ContactsPage.tsx
@@ -4,6 +4,8 @@ import {SearchField} from '@ui/components/contacts/SearchField';
 import {btnClickHandler, clickHandler, clickHandlerVoid} from '@aliases/ui';
 import {useDispatch, useSelector} from 'react-redux';
 import {AuthActions} from '@store/actions/AuthActions';
+// import {MessagesActions} from '@store/actions/MessagesActions';
+import {ContactsActions} from '@store/actions/ContactsActions';
 import {IRootState} from '@interfaces/IStore';
 import {IContactCardProps} from '@interfaces/IProps';
 import {IUser} from '@interfaces/IUser';
@@ -13,18 +15,45 @@ import {IMessage} from '@interfaces/IMessage';
 interface IContactsPageState {
     settingsModalShow: boolean;
     addModalShow: boolean;
-    cards: IContactCardProps[];
 }
 
 export const ContactsPage: React.FC = () => {
     const dispatch = useDispatch();
+
+    // useEffect(() => {dispatch(MessagesActions.getAllMessagesThunk());}, []);
+
+    const user = useSelector<IRootState, IUser | null>(state => state.Auth.user);
     const contacts = useSelector<IRootState, IUser[]>(state => state.Contacts.contacts);
     const messages = useSelector<IRootState, IMessage[]>(state => state.Messages);
+    const getLastMessage = (userId: number): IMessage => {
+        const messageList = messages.filter(message => {
+            const fromUser: boolean = message.fromUserId === userId;
+            const toUser: boolean = message.toUserId === userId;
+            return fromUser || toUser;
+        });
+        const lastIndex: number = messageList.length - 1;
+        return messageList[lastIndex];
+    };
+
+    const cards: IContactCardProps[] = contacts
+        .map(contact => ({
+            username: contact.username,
+            avatarSrc: contact.avatarSrc,
+            online: contact.online,
+            userId: contact.id,
+            lastMessageFromUserId: getLastMessage(contact.id).fromUserId,
+            userAvatarSrc: user!.avatarSrc,
+            lastMessage: getLastMessage(contact.id).text,
+            timeOfLastMessage: new Date(getLastMessage(contact.id).date),
+            onClick: () => { 
+                dispatch(ContactsActions.setCurrentContact(contact));
+            }
+        }))
+        .sort((a, b) => b.timeOfLastMessage.getTime() - a.timeOfLastMessage.getTime());
     
     const initialState: IContactsPageState = {
         settingsModalShow: false,
         addModalShow: false,
-        cards: []
     };
     const [state, setState] = useState(initialState);
 
@@ -69,7 +98,7 @@ export const ContactsPage: React.FC = () => {
                 onAddUserOk={handleAddUserOk}
             />
             <ContactCards 
-                cards={state.cards}
+                cards={cards}
             />
         </>
     )
