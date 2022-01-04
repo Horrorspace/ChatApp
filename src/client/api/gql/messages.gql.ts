a3fa5d879bdaefa999339adf1c05738e70dc54d1
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 19:57:40 +0700

    add IMessageRes interface

--- src/client/api/gql/messages.gql.ts
+++ src/client/api/gql/messages.gql.ts
@@ -2,25 +2,34 @@ import {gql, ApolloQueryResult} from '@apollo/client';
 import {AbstractGql} from '@api/gql/abstract.gql';
 import {IMessage} from '@interfaces/IMessage';
 
+interface IMessageRes {
+    messages: IMessage[];
+}
 
 export class MessagesGql extends AbstractGql {
-    private static async getAllMessagesQuery(): Promise<ApolloQueryResult<IMessage[]>> {
-        return await MessagesGql.client.query({
-            query: gql`
-            query getMessages {
-                messages {
-                    id
-                    toUserId
-                    fromUserId
-                    text
-                    date
-                    readed
-                }
-            }`
-        })
+    private static async getAllMessagesQuery(): Promise<ApolloQueryResult<IMessageRes>> {
+        try {
+            const res = await MessagesGql.client.query<IMessageRes>({
+                query: gql`
+                query getMessages {
+                    messages {
+                        id
+                        toUserId
+                        fromUserId
+                        text
+                        date
+                        readed
+                    }
+                }`
+            });
+            return res;
+        }
+        catch(error) {
+            throw error;
+        }
     }
   
-    public static async getAllMessages(): Promise<IMessage[]> {
-        return await MessagesGql.makeRequest<void, IMessage[]>(MessagesGql.getAllMessagesQuery, undefined);
+    public static async getAllMessages(): Promise<IMessageRes> {
+        return await MessagesGql.makeRequest<void, IMessageRes>(MessagesGql.getAllMessagesQuery, undefined);
     }
 }
