6b5dba348b5181b6d9350b1e748b166899ffe72c
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-04 20:00:29 +0700

    remove loading, add userLoading and tokenLoading

--- src/client/core/dto/auth.dto.ts
+++ src/client/core/dto/auth.dto.ts
@@ -6,16 +6,19 @@ import {IUser} from '@interfaces/IUser';
 export class AuthDto implements IAuthState {
     public readonly user: IUser | null;
     public readonly token: IToken | null;
-    public readonly loading: boolean;
+    public readonly userLoading: boolean;
+    public readonly tokenLoading: boolean;
 
 
     constructor(
         user: IUser | null,
         token: IToken | null,
-        loading: boolean
+        userLoading: boolean,
+        tokenLoading: boolean
     ) {
         this.user = user;
         this.token = token;
-        this.loading = loading;
+        this.userLoading = userLoading;
+        this.tokenLoading = tokenLoading;
     }
 }
