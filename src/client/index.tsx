28261461f3e01a9fe50a29fcd8202f3e14bd83ef
Utkin Eugeniy <utkineugeniy@gmail.com>
2022-01-06 20:01:00 +0700

    edit start

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,10 +4,7 @@ import {render} from 'react-dom';
 import {App} from '@ui/App';
 import {Store} from '@store/Store';
 import {AuthActions} from '@store/actions/AuthActions';
-import {MessagesActions} from '@store/actions/MessagesActions';
-import {UsersActions} from '@store/actions/UsersActions';
 import {AuthREST} from '@api/REST/auth.rest';
-import {MessagesSocket} from '@api/socket/messages.socket';
 
 
 // const user: ILogin = new LoginDto('tester3111@test.com', '1111');
@@ -17,15 +14,17 @@ async function start() {
         const user = await AuthREST.getUser();
         const token = await AuthREST.getToken();
         Store.start();
-        Store.value.dispatch(AuthActions.setAuth(user));
-        Store.value.dispatch(AuthActions.setToken(token));
-        Store.value.dispatch(MessagesActions.getAllMessagesThunk());
-        Store.value.dispatch(UsersActions.getUsersThunk());
-        MessagesSocket.start();
+        if(user) {
+            Store.value.dispatch(AuthActions.setAuth(user));
+        }
+        if(token) {
+            Store.value.dispatch(AuthActions.setToken(token));
+        }
         render(<App />, document.getElementById('root'));
     }
     catch(error) {
         console.error(error);
+        Store.start();
         render(<App />, document.getElementById('root'));
     }
 }
